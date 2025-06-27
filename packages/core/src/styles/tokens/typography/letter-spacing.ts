ff6e9b66605abb972cc901be0ebd3fd8ef99806d
ZeroChoi2781 <zero.choi@goorm.io>
2025-06-27 16:31:16 +0900

    feat: letter-spacing에 scale factor 추가

--- packages/core/src/styles/tokens/typography/letter-spacing.ts
+++ packages/core/src/styles/tokens/typography/letter-spacing.ts
@@ -1,7 +1,11 @@
+import { calc } from '@vanilla-extract/css-utils';
+
+import { scaleFactorVar } from '~/styles/global.css';
+
 export const LETTER_SPACING = {
-    '000': '0',
-    '100': '-0.1px',
-    '200': '-0.2px',
-    '300': '-0.3px',
-    '400': '-0.4px',
+    '000': calc.multiply(scaleFactorVar, '0'),
+    '100': calc.multiply(scaleFactorVar, '-0.1px'),
+    '200': calc.multiply(scaleFactorVar, '-0.2px'),
+    '300': calc.multiply(scaleFactorVar, '-0.3px'),
+    '400': calc.multiply(scaleFactorVar, '-0.4px'),
 };
