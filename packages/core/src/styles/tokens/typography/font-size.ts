bce47c570c3ed7bc8b6bb739d8af4feca48a7e55
ZeroChoi2781 <zero.choi@goorm.io>
2025-06-27 16:30:23 +0900

    feat: font-size, line-height에 scale factor 추가

--- packages/core/src/styles/tokens/typography/font-size.ts
+++ packages/core/src/styles/tokens/typography/font-size.ts
@@ -1,15 +1,19 @@
+import { calc } from '@vanilla-extract/css-utils';
+
+import { scaleFactorVar } from '~/styles/global.css';
+
 export const FONT_SIZE = {
-    '025': '10px',
-    '050': '12px',
-    '075': '14px',
-    '100': '16px',
-    '200': '18px',
-    '300': '20px',
-    '400': '24px',
-    '500': '32px',
-    '600': '38px',
-    '700': '48px',
-    '800': '64px',
-    '900': '80px',
-    '1000': '120px',
+    '025': calc.multiply(scaleFactorVar, '10px'),
+    '050': calc.multiply(scaleFactorVar, '12px'),
+    '075': calc.multiply(scaleFactorVar, '14px'),
+    '100': calc.multiply(scaleFactorVar, '16px'),
+    '200': calc.multiply(scaleFactorVar, '18px'),
+    '300': calc.multiply(scaleFactorVar, '20px'),
+    '400': calc.multiply(scaleFactorVar, '24px'),
+    '500': calc.multiply(scaleFactorVar, '32px'),
+    '600': calc.multiply(scaleFactorVar, '38px'),
+    '700': calc.multiply(scaleFactorVar, '48px'),
+    '800': calc.multiply(scaleFactorVar, '64px'),
+    '900': calc.multiply(scaleFactorVar, '80px'),
+    '1000': calc.multiply(scaleFactorVar, '120px'),
 };
