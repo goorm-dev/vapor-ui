90743dcde64b9ac288ef92ace7f89790cb23d7dc
Yunsup Sim <dev@ethansup.net>
2025-06-30 14:05:58 +0900

    fix(eslint-config): Change eslint-plugin-import to eslint-plugin-import-x

--- packages/eslint-config/import.js
+++ packages/eslint-config/import.js
@@ -1,11 +1,12 @@
 // @ts-check
 import { rules } from './libs/import-rule.js';
 import { settings } from './libs/import-setting.js';
-// @ts-ignore
-import imports from 'eslint-plugin-import';
+import { importX } from 'eslint-plugin-import-x';
 
-/** @type {Record<string, import('eslint').ESLint.Plugin>} */
-export const plugins = { import: imports };
+/**
+ * @description https://github.com/typescript-eslint/typescript-eslint/issues/10935 If this issue closed, we can remove any type.
+ * @type {Record<string, any>} */
+export const plugins = { import: importX };
 
 export { rules };
 
