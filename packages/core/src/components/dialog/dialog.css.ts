a45e05362ef01696ac4411892b5b760414940267
noah.choi <noah.choi@goorm.io>
2025-06-30 16:13:53 +0900

    refactor: add component layer on dialog.css

--- packages/core/src/components/dialog/dialog.css.ts
+++ packages/core/src/components/dialog/dialog.css.ts
@@ -1,7 +1,8 @@
-import { keyframes, style } from '@vanilla-extract/css';
+import { keyframes } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
 import { vars } from '~/styles/contract.css';
+import { layerStyle } from '~/styles/layers.css';
 
 const fadeIn = keyframes({
     '0%': { opacity: 0 },
@@ -13,7 +14,7 @@ const fadeOut = keyframes({
     '100%': { opacity: 0 },
 });
 
-export const overlay = style({
+export const overlay = layerStyle('component', {
     position: 'fixed',
     inset: 0,
     transition: 'opacity 0.2s ease-out',
@@ -36,7 +37,7 @@ export const slideDown = keyframes({
 });
 
 export const content = recipe({
-    base: {
+    base: layerStyle('component', {
         position: 'fixed',
         top: '50%',
         left: '50%',
@@ -61,19 +62,19 @@ export const content = recipe({
                 animation: `${slideDown} 0.2s ease-out forwards`,
             },
         },
-    },
+    }),
 
     defaultVariants: { size: 'md' },
     variants: {
         size: {
-            md: { width: '31.25rem' },
-            lg: { width: '50rem' },
-            xl: { width: '71.25rem' },
+            md: layerStyle('component', { width: '31.25rem' }),
+            lg: layerStyle('component', { width: '50rem' }),
+            xl: layerStyle('component', { width: '71.25rem' }),
         },
     },
 });
 
-export const title = style({
+export const title = layerStyle('component', {
     lineHeight: vars.typography.lineHeight['200'],
     letterSpacing: vars.typography.letterSpacing['100'],
     color: vars.color.foreground['normal'],
@@ -81,7 +82,7 @@ export const title = style({
     fontWeight: vars.typography.fontWeight['700'],
 });
 
-export const description = style({
+export const description = layerStyle('component', {
     lineHeight: vars.typography.lineHeight['075'],
     letterSpacing: vars.typography.letterSpacing['100'],
     color: vars.color.foreground['normal'],
@@ -89,7 +90,7 @@ export const description = style({
     fontWeight: vars.typography.fontWeight['400'],
 });
 
-export const header = style({
+export const header = layerStyle('component', {
     display: 'flex',
     alignItems: 'center',
     gap: vars.size.space['150'],
@@ -99,14 +100,14 @@ export const header = style({
     height: vars.size.dimension['700'],
 });
 
-export const body = style({
+export const body = layerStyle('component', {
     paddingBlock: 0,
     paddingInline: vars.size.space['300'],
     width: '100%',
     overflowY: 'auto',
 });
 
-export const footer = style({
+export const footer = layerStyle('component', {
     display: 'flex',
     alignItems: 'center',
     paddingBlock: vars.size.space['200'],
