244e44e497bd10e155a29d35e4865b19572df9b3
noah.choi <noah.choi@goorm.io>
2025-07-01 18:05:27 +0900

    chore: remove duplicated avatar testbed

--- packages/core/src/components/avatar/avatar.stories.tsx
+++ packages/core/src/components/avatar/avatar.stories.tsx
@@ -1,6 +1,7 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
 import { Avatar } from '.';
 import { Flex } from '../flex';
-import type { Meta, StoryObj } from '@storybook/react';
 
 export default {
     title: 'Avatar',
@@ -122,91 +123,6 @@ export const TestBed: Story = {
                 <Avatar.Simple
                     size="xl"
                     src="https://avatars.githubusercontent.com/u/217160984?v=4"
-                    {...args}
-                    alt="hi"
-                />
-            </Flex>
-            <Flex>
-                <Avatar.Simple
-                    size="sm"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="1"
-                />
-                <Avatar.Simple
-                    size="md"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="2"
-                />
-                <Avatar.Simple
-                    size="lg"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="3"
-                />
-                <Avatar.Simple
-                    size="xl"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="4"
-                />
-            </Flex>
-            <Flex>
-                <Avatar.Simple
-                    size="sm"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="noah.choi"
-                />
-                <Avatar.Simple
-                    size="md"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="noah.choi"
-                />
-                <Avatar.Simple
-                    size="lg"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="noah.choi"
-                />
-                <Avatar.Simple
-                    size="xl"
-                    src="https://images.unsplashd.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="noah.choi"
-                />
-            </Flex>
-        </>
-    ),
-};
-
-export const TestBed: Story = {
-    render: (args) => (
-        <>
-            <Flex>
-                <Avatar.Simple
-                    size="sm"
-                    src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="hi"
-                />
-                <Avatar.Simple
-                    size="md"
-                    src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="hi"
-                />
-                <Avatar.Simple
-                    size="lg"
-                    src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-                    {...args}
-                    alt="hi"
-                />
-                <Avatar.Simple
-                    size="xl"
-                    src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
                     {...args}
                     alt="hi"
                 />
