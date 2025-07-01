588b032becc4a6e696a0ee468785582ad40cc466
noah.choi <noah.choi@goorm.io>
2025-07-02 08:28:53 +0900

    chore: adjust .Root pattern

--- apps/website/src/components/demo/examples/assemble-component.tsx
+++ apps/website/src/components/demo/examples/assemble-component.tsx
@@ -1,14 +1,14 @@
-import { Card } from "@vapor-ui/core";
+import { Card } from '@vapor-ui/core';
 
 export default function AssembleComponent() {
-  return (
-    <Card>
-      <Card.Header>
-        <h2>Assemble Component</h2>
-      </Card.Header>
-      <Card.Body>
-        <p>This is an example of assembling a component using Vapor UI.</p>
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card.Root>
+            <Card.Header>
+                <h2>Assemble Component</h2>
+            </Card.Header>
+            <Card.Body>
+                <p>This is an example of assembling a component using Vapor UI.</p>
+            </Card.Body>
+        </Card.Root>
+    );
 }
