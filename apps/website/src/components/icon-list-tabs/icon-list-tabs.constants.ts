94e13d4ddd3f80479db02ce2c99a6eb70a180c60
ChanHyung-Lee <max.lee@goorm.io>
2025-07-01 16:53:48 +0900

    feat(icons): add 'symbol-black' category to icon list and update icon assignment logic

--- apps/website/src/components/icon-list-tabs/icon-list-tabs.constants.ts
+++ apps/website/src/components/icon-list-tabs/icon-list-tabs.constants.ts
@@ -3,7 +3,7 @@ import type { IconType } from '@vapor-ui/icons';
 
 import { SYMBOL_ICONS } from '~/constants/icon';
 
-export const ICON_LIST = ['basic', 'outline', 'symbol'] as const;
+export const ICON_LIST = ['basic', 'outline', 'symbol', 'symbol-black'] as const;
 
 const initialVaporIcons: {
     [key in (typeof ICON_LIST)[number]]: { [key: string]: IconType };
@@ -11,6 +11,7 @@ const initialVaporIcons: {
     basic: {},
     outline: {},
     symbol: {},
+    'symbol-black': {},
 };
 
 // Create a Set of SymbolIcons constructor references for fast lookup
@@ -19,7 +20,11 @@ const symbolIconSet = new Set(SYMBOL_ICONS);
 export const VAPOR_ICONS = Object.entries(allIcons).reduce((acc, [key, value]) => {
     if (typeof value === 'function') {
         if (symbolIconSet.has(value)) {
-            acc.symbol[key] = value;
+            if (key.endsWith('ColorIcon')) {
+                acc.symbol[key] = value;
+            } else {
+                acc['symbol-black'][key] = value;
+            }
         } else if (key.endsWith('OutlineIcon')) {
             acc.outline[key] = value;
         } else if (key.endsWith('Icon')) {
