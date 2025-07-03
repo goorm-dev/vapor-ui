85567401b328b71edaa8440048a9a0c283e08fc2
ChanHyung-Lee <max.lee@goorm.io>
2025-07-03 11:13:17 +0900

    refactor: replace image components with icon components for navigation links

--- apps/website/src/constants/site-links.tsx
+++ apps/website/src/constants/site-links.tsx
@@ -1,50 +1,23 @@
+import { DiscordColorIcon, FigmaColorIcon, GithubIcon } from '@vapor-ui/icons';
 import type { LinkItemType } from 'fumadocs-ui/layouts/links';
-import Image from 'next/image';
 
 export const navLinks = [
     {
-        icon: (
-            <div className="relative inline-block w-5 h-5">
-                <Image
-                    src="/icons/figma-color.svg"
-                    alt="Figma"
-                    fill
-                    style={{ objectFit: 'contain' }}
-                />
-            </div>
-        ),
+        icon: <FigmaColorIcon size={20} />,
         text: 'Figma',
         url: 'https://www.figma.com/community/file/1508829832204351721/vapor-design-system',
         label: 'Vapor figma comunity file',
         type: 'icon',
     },
     {
-        icon: (
-            <div className="relative inline-block w-5 h-5">
-                <Image
-                    src="/icons/discord-color.svg"
-                    alt="Discord"
-                    fill
-                    style={{ objectFit: 'contain' }}
-                />
-            </div>
-        ),
+        icon: <DiscordColorIcon size={20} />,
         text: 'Discord',
         url: 'https://discord.gg/7Z8Ecur63D',
         label: 'Vapor Discord comunity',
         type: 'icon',
     },
     {
-        icon: (
-            <div className="relative inline-block w-5 h-5">
-                <Image
-                    src="/icons/github-color.svg"
-                    alt="Github"
-                    fill
-                    style={{ objectFit: 'contain' }}
-                />
-            </div>
-        ),
+        icon: <GithubIcon size={20} />,
         text: 'Github',
         url: 'https://github.com/goorm-dev/vapor-ui',
         label: 'Vapor Github',
