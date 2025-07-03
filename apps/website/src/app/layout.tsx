2d921185c3f04cad8b45b9332ddc4030f5202cff
ChanHyung-Lee <max.lee@goorm.io>
2025-07-03 10:08:37 +0900

    feat: update theme storage keys for layout components

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import type { ReactNode } from 'react';
 
 import { ThemeProvider, ThemeScript } from '@vapor-ui/core';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
+import { usePathname } from 'next/navigation';
 
 import DefaultSearchDialog from '~/components/search/search';
 
@@ -13,12 +16,14 @@ const inter = Inter({
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
+    const pathname = usePathname();
     return (
         <html lang="ko" className={inter.className} suppressHydrationWarning>
             <head>
                 <ThemeScript
                     config={{
                         appearance: 'light',
+                        storageKey: pathname === '/' ? 'theme-home' : `theme-index`,
                     }}
                 />
                 <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -33,6 +38,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                     <ThemeProvider
                         config={{
                             appearance: 'light',
+                            storageKey: `theme-index`,
                         }}
                     >
                         {children}
