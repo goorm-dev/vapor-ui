6d38c965470cb42a6b92b67b08bb2ebc17cfe891
MaxLee <126851396+MaxLee-dev@users.noreply.github.com>
2025-07-07 17:56:56 +0900

    docs: Sidebar navigation structure (#59)
    * refactor: simplify SiteNavBar by removing unused navItems and adding direct link to Docs
    
    * docs: update documentation structure by adding 'Getting Started' section and removing 'Overview' section; include installation and theming guides
    
    * docs: update links in 'Getting Started' section to reflect new documentation structure

--- apps/website/src/components/foundation-list/foundation-list.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './foundation-list.module.scss';
-
-const FoundationList = ({ children }: { children: React.ReactNode }) => {
-    return <div className={styles.container}>{children}</div>;
-};
-
-export default FoundationList;
