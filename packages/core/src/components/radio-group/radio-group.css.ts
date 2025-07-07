f1e43ae5d56ea38adc4f618fd69b2431c64cd71b
noah.choi <noah.choi@goorm.io>
2025-07-07 18:24:13 +0900

    feat: add radio group

---
+++ packages/core/src/components/radio-group/radio-group.css.ts
@@ -0,0 +1,123 @@
+import { recipe } from '@vanilla-extract/recipes';
+
+import { vars } from '~/styles/contract.css';
+import { layerStyle } from '~/styles/layers.css';
+import { interaction } from '~/styles/mixins/interactions.css';
+import { visuallyHidden } from '~/styles/mixins/visually-hidden.css';
+
+export const root = recipe({
+    base: layerStyle('component', {
+        display: 'flex',
+        gap: vars.size.space['050'],
+    }),
+
+    defaultVariants: { orientation: 'vertical' },
+    variants: {
+        orientation: {
+            horizontal: layerStyle('component', {
+                flexDirection: 'row',
+                gap: vars.size.space['100'],
+            }),
+            vertical: layerStyle('component', {
+                flexDirection: 'column',
+                gap: vars.size.space['050'],
+            }),
+        },
+    },
+});
+
+export const item = recipe({
+    base: layerStyle('component', {
+        display: 'flex',
+        flexShrink: 0,
+        alignItems: 'center',
+
+        gap: vars.size.space['100'],
+    }),
+
+    defaultVariants: { disabled: false },
+    variants: {
+        disabled: { true: layerStyle('component', { opacity: 0.32, pointerEvents: 'none' }) },
+    },
+});
+
+export const control = recipe({
+    base: [
+        interaction(),
+        layerStyle('component', {
+            position: 'relative',
+
+            display: 'flex',
+            flexShrink: 0,
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: vars.size.space[100],
+
+            border: `0.0625rem solid ${vars.color.border.normal}`,
+            borderRadius: 9999,
+
+            backgroundColor: vars.color.background.normal,
+            cursor: 'pointer',
+
+            padding: vars.size.space['000'],
+
+            selectors: {
+                '&[data-state="checked"]': {
+                    backgroundColor: vars.color.background.primary,
+                },
+            },
+        }),
+    ],
+
+    defaultVariants: {
+        invalid: false,
+        size: 'md',
+    },
+
+    variants: {
+        invalid: {
+            true: layerStyle('component', { borderColor: vars.color.background['danger'] }),
+        },
+
+        size: {
+            md: layerStyle('component', {
+                width: vars.size.dimension[200],
+                height: vars.size.dimension[200],
+            }),
+            lg: layerStyle('component', {
+                width: vars.size.dimension[300],
+                height: vars.size.dimension[300],
+            }),
+        },
+    },
+});
+
+export const indicator = layerStyle('component', {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    border: `0.0625rem solid ${vars.color.border.normal}`,
+    borderRadius: '9999px',
+    backgroundColor: vars.color.background.normal,
+    width: '50%',
+    height: '50%',
+});
+
+export const label = recipe({
+    base: layerStyle('component', {
+        lineHeight: vars.typography.lineHeight['075'],
+
+        letterSpacing: vars.typography.letterSpacing['100'],
+        color: vars.color.foreground['normal'],
+        fontSize: vars.typography.fontSize['075'],
+        fontWeight: vars.typography.fontWeight[400],
+    }),
+
+    defaultVariants: { visuallyHidden: false },
+    variants: {
+        visuallyHidden: {
+            true: visuallyHidden,
+        },
+    },
+});
---
+++ packages/core/src/components/radio-group/radio-group.tsx
@@ -0,0 +1,200 @@
+import type { ComponentPropsWithoutRef } from 'react';
+import { forwardRef, useId } from 'react';
+
+import {
+    Indicator as RadixIndicator,
+    Item as RadixItem,
+    Root as RadixRoot,
+} from '@radix-ui/react-radio-group';
+import clsx from 'clsx';
+
+import { createContext } from '~/libs/create-context';
+import { vapor } from '~/libs/factory';
+import type { MergeRecipeVariants } from '~/libs/recipe';
+import { createSplitProps } from '~/utils/create-split-props';
+
+import * as styles from './radio-group.css';
+
+type RadixRootProps = ComponentPropsWithoutRef<typeof RadixRoot>;
+type PrimitiveRootProps = Pick<
+    RadixRootProps,
+    'name' | 'dir' | 'loop' | 'value' | 'onValueChange' | 'defaultValue' | 'required' | 'disabled'
+>;
+
+type RadioGroupVariants = MergeRecipeVariants<
+    typeof styles.root | typeof styles.label | typeof styles.control
+>;
+type RadioGroupSharedProps = RadioGroupVariants & PrimitiveRootProps;
+type RadioGroupContext = RadioGroupSharedProps;
+
+const [RadioGroupProvider, useRadioGroupContext] = createContext<RadioGroupContext>({
+    name: 'RadioGroup',
+    hookName: 'useRadioGroupContext',
+    providerName: 'RadioGroupProvider',
+});
+
+/* -------------------------------------------------------------------------------------------------
+ * RadioGroup.Root
+ * -----------------------------------------------------------------------------------------------*/
+
+type RadioGroupRootPrimitiveProps = ComponentPropsWithoutRef<typeof vapor.div>;
+interface RadioGroupRootProps
+    extends Omit<RadioGroupRootPrimitiveProps, keyof PrimitiveRootProps>,
+        PrimitiveRootProps {}
+
+const Root = forwardRef<HTMLDivElement, RadioGroupRootProps>(({ className, ...props }, ref) => {
+    const [sharedProps, otherProps] = createSplitProps<RadioGroupSharedProps>()(props, [
+        'name',
+        'required',
+        'disabled',
+        'value',
+        'onValueChange',
+        'defaultValue',
+        'dir',
+        'loop',
+        'orientation',
+        'invalid',
+        'size',
+        'visuallyHidden',
+    ]);
+
+    const { orientation } = sharedProps;
+
+    return (
+        <RadioGroupProvider value={sharedProps}>
+            <RadixRoot
+                ref={ref}
+                className={clsx(styles.root({ orientation }), className)}
+                {...sharedProps}
+                {...otherProps}
+            />
+        </RadioGroupProvider>
+    );
+});
+Root.displayName = 'RadioGroup.Root';
+
+/* -------------------------------------------------------------------------------------------------
+ * RadioGroup.Item
+ * -----------------------------------------------------------------------------------------------*/
+
+type RadioGroupItemPrimitiveProps = ComponentPropsWithoutRef<typeof vapor.div>;
+
+type RadioGroupItemVariants = MergeRecipeVariants<typeof styles.control | typeof styles.label>;
+type RadioGroupItemSharedProps = RadioGroupItemVariants &
+    Pick<RadioGroupControlPrimitiveProps, 'value' | 'disabled'>;
+
+type RadioGroupItemContext = RadioGroupItemSharedProps & {
+    radioGroupItemId?: string;
+};
+
+const [RadioGroupItemProvider, useRadioGroupItemContext] = createContext<RadioGroupItemContext>({
+    name: 'RadioGroupItem',
+    hookName: 'useRadioGroupItemContext',
+    providerName: 'RadioGroupItemProvider',
+});
+
+interface RadioGroupItemProps extends RadioGroupItemPrimitiveProps, RadioGroupItemSharedProps {}
+
+const Item = forwardRef<HTMLDivElement, RadioGroupItemProps>(({ className, ...props }, ref) => {
+    const radioGroupItemId = useId();
+    const rootContext = useRadioGroupContext();
+
+    const [itemProps, otherProps] = createSplitProps<RadioGroupItemSharedProps>()(props, [
+        'value',
+        'disabled',
+        'visuallyHidden',
+        'size',
+        'invalid',
+        'disabled',
+    ]);
+
+    const {
+        disabled = rootContext.disabled,
+        invalid = rootContext.invalid,
+        visuallyHidden = rootContext.visuallyHidden,
+    } = itemProps;
+
+    return (
+        <RadioGroupItemProvider
+            value={{ ...itemProps, radioGroupItemId, disabled, invalid, visuallyHidden }}
+        >
+            <vapor.div
+                ref={ref}
+                className={clsx(styles.item({ disabled }), className)}
+                {...otherProps}
+            />
+        </RadioGroupItemProvider>
+    );
+});
+Item.displayName = 'RadioGroup.Item';
+
+/* -------------------------------------------------------------------------------------------------
+ * RadioGroup.Control
+ * -----------------------------------------------------------------------------------------------*/
+
+type RadioGroupControlPrimitiveProps = ComponentPropsWithoutRef<typeof RadixItem>;
+
+interface RadioGroupControlProps
+    extends Omit<RadioGroupControlPrimitiveProps, keyof RadioGroupItemSharedProps> {}
+
+const Control = forwardRef<HTMLButtonElement, RadioGroupControlProps>(
+    ({ id, className, ...props }, ref) => {
+        const { size } = useRadioGroupContext();
+        const { radioGroupItemId, value, invalid, disabled } = useRadioGroupItemContext();
+
+        return (
+            <RadixItem
+                ref={ref}
+                id={id || radioGroupItemId}
+                value={value}
+                disabled={disabled}
+                aria-invalid={invalid}
+                className={clsx(styles.control({ size, invalid }))}
+                {...props}
+            >
+                <RadixIndicator className={clsx(styles.indicator, className)} />
+            </RadixItem>
+        );
+    },
+);
+Control.displayName = 'RadioGroup.Control';
+
+/* -------------------------------------------------------------------------------------------------
+ * RadioGroup.ItemText
+ * -----------------------------------------------------------------------------------------------*/
+
+type PrimitiveLabelProps = ComponentPropsWithoutRef<typeof vapor.label>;
+interface RadioGroupLabelProps extends Omit<PrimitiveLabelProps, keyof RadioGroupItemSharedProps> {}
+
+const Label = forwardRef<HTMLLabelElement, RadioGroupLabelProps>(
+    ({ htmlFor, className, ...props }, ref) => {
+        const { radioGroupItemId, visuallyHidden } = useRadioGroupItemContext();
+
+        return (
+            <vapor.label
+                ref={ref}
+                htmlFor={htmlFor || radioGroupItemId}
+                className={clsx(styles.label({ visuallyHidden }), className)}
+                {...props}
+            />
+        );
+    },
+);
+Label.displayName = 'RadioGroup.Label';
+
+/* -----------------------------------------------------------------------------------------------*/
+
+export {
+    Root as RadioGroupRoot,
+    Item as RadioGroupItem,
+    Control as RadioGroupControl,
+    Label as RadioGroupLabel,
+};
+export type {
+    RadioGroupRootProps,
+    RadioGroupItemProps,
+    RadioGroupControlProps,
+    RadioGroupLabelProps,
+};
+
+export const RadioGroup = { Root, Item, Control, Label };
