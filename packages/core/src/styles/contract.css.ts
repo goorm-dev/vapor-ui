bfbb4f2852a7de1cfdc29400680196100f31a3d7
ZeroChoi2781 <zero.choi@goorm.io>
2025-07-07 10:55:57 +0900

    fix: global.css import 제거

--- packages/core/src/styles/contract.css.ts
+++ packages/core/src/styles/contract.css.ts
@@ -1,6 +1,5 @@
 import { createGlobalThemeContract } from '@vanilla-extract/css';
 
 import { THEME_TOKENS } from './constants';
-import './global.css';
 
 export const vars = createGlobalThemeContract(THEME_TOKENS, (_, path) => `vapor-${path.join('-')}`);
