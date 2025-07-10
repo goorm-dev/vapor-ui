900618e9fbcc92e2f2779acfd3ea1b51fc313f4a
ZeroChoi2781 <zero.choi@goorm.io>
2025-07-10 18:12:33 +0900

    feat: tsup config 설정 추가

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -115,6 +115,8 @@ export default defineConfig([
             css: '@layer vapor-theme, vapor-reset, vapor-component, vapor-utilities;',
         },
     },
+
+    // Types build
     {
         entry: ['src/index.ts', 'src/components/*/index.ts'],
         outDir: DIR,
@@ -129,6 +131,7 @@ export default defineConfig([
     {
         entry: {
             styles: 'src/styles/index.ts',
+            tailwind: 'src/styles/tailwind-preset.css.ts',
         },
         outDir: DIR,
         esbuildPlugins: [
