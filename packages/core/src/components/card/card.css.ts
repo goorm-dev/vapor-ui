2fddbe8f6238311a0819a23204543656940baded
ZeroChoi2781 <zero.choi@goorm.io>
2025-07-29 13:56:51 +0900

    feat: layer 명칭 `vapor-*` 제거

--- packages/core/src/components/card/card.css.ts
+++ packages/core/src/components/card/card.css.ts
@@ -1,7 +1,7 @@
 import { layerStyle } from '~/styles/utils/layer-style.css';
 import { vars } from '~/styles/vars.css';
 
-export const root = layerStyle('vapor-component', {
+export const root = layerStyle('components', {
     display: 'flex',
     flexDirection: 'column',
 
@@ -11,17 +11,17 @@ export const root = layerStyle('vapor-component', {
     backgroundColor: vars.color.background['normal-lighter'],
 });
 
-export const header = layerStyle('vapor-component', {
+export const header = layerStyle('components', {
     borderBottom: `1px solid ${vars.color.border.normal}`,
 
     padding: `${vars.size.space[200]} ${vars.size.space[300]}`,
 });
 
-export const body = layerStyle('vapor-component', {
+export const body = layerStyle('components', {
     padding: vars.size.space[300],
 });
 
-export const footer = layerStyle('vapor-component', {
+export const footer = layerStyle('components', {
     borderTop: `1px solid ${vars.color.border.normal}`,
 
     padding: `${vars.size.space[200]} ${vars.size.space[300]}`,
