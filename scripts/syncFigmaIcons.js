e373c35e75e8e0eb72df51b744d3308b8a3dfc22
ChanHyung-Lee <max.lee@goorm.io>
2025-07-29 14:34:07 +0900

    feat: Integrate Figma Icons Sync Workflow and Add Figma API Utilities
    - Introduced a GitHub Actions workflow to sync icons from Figma, triggered manually or on a schedule.
    - Added scripts for fetching and processing icons from Figma, including utilities for handling SVGs and AWS S3 uploads.
    - Created constants for Figma API keys and node IDs to streamline icon management.
    - Implemented utility functions for file operations and removing duplicate imports in icon files.
    - Updated package.json to include new dependencies: axios, bluebird, dotenv, and lodash-es.

---
+++ scripts/syncFigmaIcons.js
@@ -0,0 +1,118 @@
+const path = require('path');
+const fs = require('fs');
+const Promise = require('bluebird');
+const { camelCase, startCase } = require('lodash-es');
+
+const getIconComponent = require('./templates/icon/IconComponent');
+const getIconComponentIndex = require('./templates/icon/iconComponentIndex');
+const getIconsIndex = require('./templates/icon/iconsIndex');
+
+const { filterDocumentByNodeType, getNodesWithUrl, getIconJsx } = require('./libs/figma');
+
+const writeFile = Promise.promisify(fs.writeFile);
+const {
+    FIGMA_ICONS_FILE_KEY,
+    FIGMA_NODE_TYPES,
+    FIGMA_ICONS_SYMBOL_COLOR_NODE_ID,
+    FIGMA_ICONS_STACK_COLOR_NODE_ID,
+    FIGMA_ICONS_RENEWAL_BRANCH_KEY,
+} = require('./constants/figma');
+const { ICON_TYPES } = require('./constants');
+
+const TYPE = process.env.TYPE;
+const CURRENT_DIRECTORY = process.cwd();
+
+const main = async () => {
+    console.log('\x1b[33m---------------- GDS FIGMA EXPORT -----------------\x1b[0m');
+
+    try {
+        const { nodeIds, targetPath } = ICON_TYPES[TYPE];
+        let FILE_KEY = FIGMA_ICONS_FILE_KEY;
+
+        // 파일 내에 COMPONENT로 설정된 node(아이콘)들을 가져온다.
+        let components = [];
+        if (TYPE === 'basic') {
+            FILE_KEY = FIGMA_ICONS_RENEWAL_BRANCH_KEY || FIGMA_ICONS_FILE_KEY;
+            // basic 아이콘은 2개의 프레임으로 구성되어 있어서 nodeIds가 배열 형태임
+            for (const nodeId of nodeIds) {
+                const nodeComponents = await filterDocumentByNodeType({
+                    nodeType: FIGMA_NODE_TYPES.COMPONENT,
+                    fileKey: FILE_KEY,
+                    nodeIds: nodeId,
+                    depth: 1,
+                });
+                components = components.concat(nodeComponents);
+            }
+        } else {
+            components = await filterDocumentByNodeType({
+                nodeType: FIGMA_NODE_TYPES.COMPONENT,
+                fileKey: FILE_KEY,
+                nodeIds,
+                depth: 1,
+            });
+        }
+
+        const componentsInfo = {
+            total: components.length,
+            nameArr: components.map(({ name }) => startCase(camelCase(name)).replace(/ /g, '')),
+        };
+        console.log(
+            `\x1b[33m GDS FIGMA EXPORT: \x1b[0m ${componentsInfo.total}개 아이콘 추출 완료`,
+        );
+
+        // 추출된 아이콘들의 id를 콤마로 구분하여, 한번에 svg 이미지들의 URL를 가져온다.
+        console.log(`\x1b[33m GDS FIGMA EXPORT: \x1b[0m svg 파일 불러오는 중...`);
+        const componentsWithUrl = await getNodesWithUrl({
+            nodes: components,
+            fileKey: FILE_KEY,
+        });
+        console.log(`\x1b[33m GDS FIGMA EXPORT: \x1b[0m svg 파일 불러오기 완료 !`);
+
+        // 이미지 URL을 통해 svg 코드를 React 컴포넌트로 변환 후, 로컬에 저장한다.
+        console.log(`\x1b[33m GDS FIGMA EXPORT: \x1b[0m React 컴포넌트로 변환 중...`);
+        const parentIconPath = path.join(CURRENT_DIRECTORY, targetPath);
+        const newIconNameArr = [];
+        const promiseCreateIcons = componentsWithUrl.map(async ({ name, url, parentId }) => {
+            const iconName = startCase(camelCase(name)).replace(/ /g, '');
+            const saveTargetPath = path.join(parentIconPath, iconName);
+
+            if (!fs.existsSync(saveTargetPath)) {
+                fs.mkdirSync(saveTargetPath);
+                newIconNameArr.push(iconName);
+            }
+
+            const isColorIcon =
+                parentId === decodeURIComponent(FIGMA_ICONS_SYMBOL_COLOR_NODE_ID) ||
+                parentId === decodeURIComponent(FIGMA_ICONS_STACK_COLOR_NODE_ID);
+
+            const iconJsx = await getIconJsx({ url, isColorIcon });
+            const IconComponent = getIconComponent(iconName, iconJsx);
+            const iconIndex = getIconComponentIndex(iconName);
+
+            const writeIconComponent = writeFile(
+                path.resolve(saveTargetPath, `${iconName}.tsx`),
+                IconComponent,
+                { encoding: 'utf8' },
+            );
+            const writeIconIndex = writeFile(path.resolve(saveTargetPath, `index.ts`), iconIndex, {
+                encoding: 'utf8',
+            });
+
+            await Promise.all([writeIconComponent, writeIconIndex]);
+        });
+        await Promise.all(promiseCreateIcons);
+        console.log(`\x1b[33m GDS FIGMA EXPORT: \x1b[0m React 컴포넌트로 변환 완료 !`);
+
+        // // entry 파일에 export
+        console.log(`\x1b[33m GDS FIGMA EXPORT: \x1b[0m entry 파일에 export 중...`);
+        const iconsIndex = getIconsIndex(componentsInfo.nameArr);
+        await writeFile(path.join(CURRENT_DIRECTORY, targetPath, 'index.ts'), iconsIndex, {
+            encoding: 'utf8',
+        });
+    } catch (err) {
+        console.error('Unhandled rejection', err);
+    }
+    console.log('\x1b[33m---------------------------------------------------\x1b[0m');
+};
+
+main();
