fe2f359d6125387a954dce96e6c2b4161992cf64
ZeroChoi2781 <zero.choi@goorm.io>
2025-07-31 14:30:40 +0900

    feat: update import order

--- packages/core/src/styles/index.ts
+++ packages/core/src/styles/index.ts
@@ -2,16 +2,20 @@
 // ⚠️ IMPORTANT: The import order is crucial to avoid circular dependencies.
 // 1. Define CSS layers (must be first)
 import './global-var.css';
+
 // 2. Global reset styles
 import './global.css';
 import './layers.css';
-import './mixins/foreground.css';
-// 5. Utility styles
-import './sprinkles.css';
-// 4. Assign theme values (after the contract is created)
-import './theme.css';
+
 // 3. Create CSS variable contract
 import './vars.css';
+
+// 4. Assign theme values (after the contract is created)
+import './theme.css';
+
+// 5. Utility styles
+import './sprinkles.css';
+import './mixins/foreground.css';
 
 
 import '../components/avatar/avatar.css';
