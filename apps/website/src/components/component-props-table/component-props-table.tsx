f5c553bf3d4832aa6c4e3ab2818d72be3df59281
ChanHyung-Lee <max.lee@goorm.io>
2025-08-04 14:27:23 +0900

    docs: remove Accessibility Table from Avatar documentation
    - Deleted the Accessibility Table section from the Avatar component documentation to streamline content.

--- apps/website/src/components/component-props-table/component-props-table.tsx
+++ apps/website/src/components/component-props-table/component-props-table.tsx
@@ -24,22 +24,22 @@ const ComponentPropsTable: React.FC<ComponentPropsTableProps> = ({ props }) => {
             <table className="w-full border-separate border-spacing-0 overflow-hidden border border-[var(--vapor-color-border-normal)] rounded-[var(--vapor-size-borderRadius-300)]">
                 <thead className="flex, items-start self-stretch rounded-[var(--vapor-size-borderRadius-300)]">
                     <tr className="bg-[var(--vapor-color-background-normal-darker)] border-b border-b-[var(--vapor-color-border-normal)]">
-                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)] text-left rounded-tl-[var(--vapor-size-borderRadius-300)]">
+                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)] text-left rounded-tl-[var(--vapor-size-borderRadius-300)] min-w-[160px]">
                             <Text typography="subtitle1" foreground="normal-lighter">
                                 Prop
                             </Text>
                         </th>
-                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)]  text-left font-medium">
+                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)] text-left font-medium">
                             <Text typography="subtitle1" foreground="normal-lighter">
                                 Type
                             </Text>
                         </th>
-                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)]  text-left ">
+                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)] text-left ">
                             <Text typography="subtitle1" foreground="normal-lighter">
                                 Default
                             </Text>
                         </th>
-                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)]  text-left ">
+                        <th className="px-[var(--vapor-size-space-300)] h-[var(--vapor-size-dimension-500)] text-left ">
                             <Text typography="subtitle1" foreground="normal-lighter">
                                 Types
                             </Text>
@@ -50,7 +50,7 @@ const ComponentPropsTable: React.FC<ComponentPropsTableProps> = ({ props }) => {
                     {props.map((prop, index) => (
                         <tr key={prop.name} className="">
                             <td
-                                className={`px-[var(--vapor-size-space-300)] py-[var(--vapor-size-space-200)] border-b border-b-[var(--vapor-color-border-normal)] ${index === props.length - 1 ? 'rounded-bl-[var(--vapor-size-borderRadius-300)] border-b-0' : ''}`}
+                                className={`px-[var(--vapor-size-space-300)] py-[var(--vapor-size-space-200)] border-b border-b-[var(--vapor-color-border-normal)] min-w-[160px] ${index === props.length - 1 ? 'rounded-bl-[var(--vapor-size-borderRadius-300)] border-b-0' : ''}`}
                             >
                                 <div className="flex items-center gap-[var(--vapor-size-space-100)]">
                                     <Text typography="body2" foreground="normal">
