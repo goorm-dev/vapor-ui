7e0ba1bcebe5942f6ff4889c85c458a4bdedff0a
noah.choi <noah.choi@goorm.io>
2025-08-05 13:37:39 +0900

    chore: split build-storybook and start playwright webServer

--- packages/core/playwright.config.ts
+++ packages/core/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     workers: 1,
 
     webServer: {
-        command: 'cd ../../ && pnpm build-storybook && npx http-server -p 9999 ./storybook-static',
+        command: 'cd ../../ && npx http-server -p 9999 ./storybook-static',
         url: BASE_URL,
         reuseExistingServer: false,
     },
