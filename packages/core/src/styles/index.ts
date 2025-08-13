e3aa21e3990c98a659a2bf77aa1a077f43608ec4
noah.choi <noah.choi@goorm.io>
2025-08-13 19:22:11 +0900

    chore: ignore formatting side-effect import (#172)

--- packages/core/src/styles/index.ts
+++ packages/core/src/styles/index.ts
@@ -1,23 +1,11 @@
-// @prettier-ignore
-// ⚠️ IMPORTANT: The import order is crucial to avoid circular dependencies.
-// 1. Define CSS layers (must be first)
+// NOTE: The import order is crucial to avoid circular dependencies between CSS files.
 import './global-var.css';
-
-// 2. Global reset styles
 import './global.css';
 import './layers.css';
-
-// 3. Create CSS variable contract
 import './vars.css';
-
-// 4. Assign theme values (after the contract is created)
 import './theme.css';
-
-// 5. Utility styles
 import './sprinkles.css';
 import './mixins/foreground.css';
-
-
 import '../components/avatar/avatar.css';
 import '../components/badge/badge.css';
 import '../components/button/button.css';
