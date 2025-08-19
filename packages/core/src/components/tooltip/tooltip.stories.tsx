53f3c0e05fb1ba4dc0ef18fdd7dccb163b7b1df4
noah.choi <noah.choi@goorm.io>
2025-08-19 17:17:13 +0900

    chore: enhance a11y of tooltip stories

--- packages/core/src/components/tooltip/tooltip.stories.tsx
+++ packages/core/src/components/tooltip/tooltip.stories.tsx
@@ -2,7 +2,9 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Tooltip } from '.';
 import { Button } from '../button';
+import { Flex } from '../flex';
 import { HStack } from '../h-stack';
+import { Text } from '../text';
 import { VStack } from '../v-stack';
 
 export default {
@@ -17,42 +19,49 @@ export default {
 } satisfies Meta<typeof Tooltip.Root>;
 
 export const Default: StoryObj<typeof Tooltip.Root> = {
-    render: (args) => (
-        <div
-            style={{
-                margin: '100px',
-                display: 'flex',
-                gap: '20px',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <Tooltip.Root side="left" {...args}>
-                <Tooltip.Trigger render={<Button>Left Tooltip</Button>} />
-                <Tooltip.Portal>
-                    <Tooltip.Content>Tooltip content</Tooltip.Content>
-                </Tooltip.Portal>
-            </Tooltip.Root>
-            <Tooltip.Root side="top" {...args}>
-                <Tooltip.Trigger render={<Button>Top Tooltip</Button>} />
-                <Tooltip.Portal>
-                    <Tooltip.Content>Tooltip content</Tooltip.Content>
-                </Tooltip.Portal>
-            </Tooltip.Root>
-            <Tooltip.Root side="bottom" {...args}>
-                <Tooltip.Trigger render={<Button>Bottom Tooltip</Button>} />
-                <Tooltip.Portal>
-                    <Tooltip.Content>Tooltip content</Tooltip.Content>
-                </Tooltip.Portal>
-            </Tooltip.Root>
-            <Tooltip.Root side="right" {...args}>
-                <Tooltip.Trigger render={<Button>Right Tooltip</Button>} />
-                <Tooltip.Portal>
-                    <Tooltip.Content>Tooltip content</Tooltip.Content>
-                </Tooltip.Portal>
-            </Tooltip.Root>
-        </div>
-    ),
+    render: (args) => {
+        return (
+            <>
+                <Flex gap="20px" margin="100px" justifyContent="center" alignItems="center">
+                    <Tooltip.Root delay={0} side="left" {...args}>
+                        <Tooltip.Trigger render={<Button>Left Tooltip</Button>} />
+                        <Tooltip.Portal>
+                            <Tooltip.Content>Tooltip content</Tooltip.Content>
+                        </Tooltip.Portal>
+                    </Tooltip.Root>
+                    <Tooltip.Root delay={0} side="top" {...args}>
+                        <Tooltip.Trigger render={<Button>Top Tooltip</Button>} />
+                        <Tooltip.Portal>
+                            <Tooltip.Content>Tooltip content</Tooltip.Content>
+                        </Tooltip.Portal>
+                    </Tooltip.Root>
+                    <Tooltip.Root delay={0} side="bottom" {...args}>
+                        <Tooltip.Trigger render={<Button>Bottom Tooltip</Button>} />
+                        <Tooltip.Portal>
+                            <Tooltip.Content>Tooltip content</Tooltip.Content>
+                        </Tooltip.Portal>
+                    </Tooltip.Root>
+                    <Tooltip.Root delay={0} side="right" {...args}>
+                        <Tooltip.Trigger render={<Button>Right Tooltip</Button>} />
+                        <Tooltip.Portal>
+                            <Tooltip.Content>Tooltip content</Tooltip.Content>
+                        </Tooltip.Portal>
+                    </Tooltip.Root>
+                </Flex>
+
+                <Flex gap="20px" margin="100px" justifyContent="center" alignItems="center">
+                    <Text>Wait for 0.5s </Text>
+
+                    <Tooltip.Root delay={500} side="bottom" {...args}>
+                        <Tooltip.Trigger render={<Button>Delayed Tooltip</Button>} />
+                        <Tooltip.Portal>
+                            <Tooltip.Content>Tooltip content</Tooltip.Content>
+                        </Tooltip.Portal>
+                    </Tooltip.Root>
+                </Flex>
+            </>
+        );
+    },
 };
 
 export const TestBed = () => {
@@ -66,25 +75,25 @@ export const TestBed = () => {
                 border="1px solid"
             >
                 <HStack margin="$800" gap="$400" justifyContent="center" alignItems="center">
-                    <Tooltip.Root open side="left">
+                    <Tooltip.Root delay={0} open side="left">
                         <Tooltip.Trigger render={<Button>Left Tooltip</Button>} />
                         <Tooltip.Portal>
                             <Tooltip.Content>Tooltip content</Tooltip.Content>
                         </Tooltip.Portal>
                     </Tooltip.Root>
-                    <Tooltip.Root open side="top">
+                    <Tooltip.Root delay={0} open side="top">
                         <Tooltip.Trigger render={<Button>Top Tooltip</Button>} />
                         <Tooltip.Portal>
                             <Tooltip.Content>Tooltip content</Tooltip.Content>
                         </Tooltip.Portal>
                     </Tooltip.Root>
-                    <Tooltip.Root open side="bottom">
+                    <Tooltip.Root delay={0} open side="bottom">
                         <Tooltip.Trigger render={<Button>Bottom Tooltip</Button>} />
                         <Tooltip.Portal>
                             <Tooltip.Content>Tooltip content</Tooltip.Content>
                         </Tooltip.Portal>
                     </Tooltip.Root>
-                    <Tooltip.Root open side="right">
+                    <Tooltip.Root delay={0} open side="right">
                         <Tooltip.Trigger render={<Button>Right Tooltip</Button>} />
                         <Tooltip.Portal>
                             <Tooltip.Content>Tooltip content</Tooltip.Content>
@@ -101,19 +110,19 @@ export const TestBed = () => {
                 border="1px solid"
             >
                 <HStack margin="$800" gap="$400" justifyContent="center" alignItems="center">
-                    <Tooltip.Root open align="start">
+                    <Tooltip.Root delay={0} open align="start">
                         <Tooltip.Trigger render={<Button>Align Start Tooltip</Button>} />
                         <Tooltip.Portal>
                             <Tooltip.Content>Tooltip content</Tooltip.Content>
                         </Tooltip.Portal>
                     </Tooltip.Root>
-                    <Tooltip.Root open align="center">
+                    <Tooltip.Root delay={0} open align="center">
                         <Tooltip.Trigger render={<Button>Align Center Tooltip</Button>} />
                         <Tooltip.Portal>
                             <Tooltip.Content>Tooltip content</Tooltip.Content>
                         </Tooltip.Portal>
                     </Tooltip.Root>
-                    <Tooltip.Root open align="end">
+                    <Tooltip.Root delay={0} open align="end">
                         <Tooltip.Trigger render={<Button>Align End Tooltip</Button>} />
                         <Tooltip.Portal>
                             <Tooltip.Content>Tooltip content</Tooltip.Content>
@@ -123,7 +132,7 @@ export const TestBed = () => {
             </VStack>
 
             <HStack margin="$800" padding="$200" border="1px solid black">
-                <Tooltip.Root open side="left">
+                <Tooltip.Root delay={0} open side="left">
                     <Tooltip.Trigger render={<Button>Left Collision</Button>} />
                     <Tooltip.Portal>
                         <Tooltip.Content>
