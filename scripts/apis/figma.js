de0a2a3dcb8b220458c54284e41f4777a0700c80
MaxLee <126851396+MaxLee-dev@users.noreply.github.com>
2025-08-19 16:39:51 +0900

    feat: Integrate Figma Icons Sync Workflow and Add Figma API Utilities (#122)

---
+++ scripts/apis/figma.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const headers = {
+    'X-FIGMA-TOKEN': process.env.FIGMA_TOKEN,
+};
+
+/**
+ * GET file nodes
+ *
+ * @link https://www.figma.com/developers/api#get-file-nodes-endpoint
+ */
+const getFileNodes = async ({ fileKey, nodeIds, depth = 1 }) => {
+    const { data } = await axios.get(
+        `https://api.figma.com/v1/files/${fileKey}/nodes?ids=${nodeIds}&depth=${depth}`,
+        { headers },
+    );
+    return data;
+};
+
+/**
+ * GET image
+ *
+ * @link https://www.figma.com/developers/api#get-images-endpoint
+ */
+const getImage = async ({ fileKey, nodeIds, format = 'svg' }) => {
+    const { data } = await axios.get(
+        `https://api.figma.com/v1/images/${fileKey}?ids=${nodeIds}&format=${format}&svg_include_id=false`,
+        {
+            headers,
+        },
+    );
+
+    return data;
+};
+
+export {
+    getFileNodes,
+    getImage,
+};
