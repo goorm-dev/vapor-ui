99d0a61b2da54dd3707fd7c7cae514430b592164
noah.choi <noah.choi@goorm.io>
2025-08-25 10:33:14 +0900

    refactor: edit implements of VComponentProps

--- packages/core/src/utils/types.ts
+++ packages/core/src/utils/types.ts
@@ -1,11 +1,16 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef, HTMLAttributes } from 'react';
 
 import type { useRender } from '@base-ui-components/react/use-render';
 
-type ColorPropToOmit<ElementType extends React.ElementType> =
-    string extends ComponentPropsWithoutRef<ElementType>['color'] ? 'color' : never;
+type OmitProp<
+    ElementType extends React.ElementType,
+    key extends keyof HTMLAttributes<ElementType>,
+> = string extends ComponentPropsWithoutRef<ElementType>[key] ? key : never;
 
 export type VComponentProps<ElementType extends React.ElementType> = Omit<
     useRender.ComponentProps<ElementType>,
-    'defaultValue' | 'dir' | ColorPropToOmit<ElementType>
+    | OmitProp<ElementType, 'defaultChecked'>
+    | OmitProp<ElementType, 'defaultValue'>
+    | OmitProp<ElementType, 'dir'>
+    | OmitProp<ElementType, 'color'>
 >;
