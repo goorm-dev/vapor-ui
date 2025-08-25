5a2739c04f1ba2adaf5d606025e55833be8f92a7
noah.choi <noah.choi@goorm.io>
2025-08-25 11:30:22 +0900

    refactor: remove default* props from VComponentProps and add Assign type

--- packages/core/src/utils/types.ts
+++ packages/core/src/utils/types.ts
@@ -1,16 +1,13 @@
-import type { ComponentPropsWithoutRef, HTMLAttributes } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
 
 import type { useRender } from '@base-ui-components/react/use-render';
 
-type OmitProp<
-    ElementType extends React.ElementType,
-    key extends keyof HTMLAttributes<ElementType>,
-> = string extends ComponentPropsWithoutRef<ElementType>[key] ? key : never;
+export type Assign<T, U> = Omit<T, keyof U> & U;
+
+type OmitColorProp<ElementType extends React.ElementType> =
+    string extends ComponentPropsWithoutRef<ElementType>['color'] ? 'color' : never;
 
 export type VComponentProps<ElementType extends React.ElementType> = Omit<
     useRender.ComponentProps<ElementType>,
-    | OmitProp<ElementType, 'defaultChecked'>
-    | OmitProp<ElementType, 'defaultValue'>
-    | OmitProp<ElementType, 'dir'>
-    | OmitProp<ElementType, 'color'>
+    OmitColorProp<ElementType>
 >;
