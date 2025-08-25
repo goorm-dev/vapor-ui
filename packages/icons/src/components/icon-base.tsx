c086780d75f8891bd9d68abbb4415097fda409ff
ChanHyung-Lee <max.lee@goorm.io>
2025-08-25 11:54:41 +0900

    refactor(icons): clean up dependencies and simplify icon components
    - Removed unused dependencies from `pnpm-lock.yaml` and `package.json`.
    - Eliminated the custom `HeartIcon` component and integrated `HeartIcon` from `@vapor-ui/icons` in the story file.
    - Streamlined the `IconBase` component by removing unnecessary CSS variable handling and props, focusing on a simpler API for width, height, and size.
    - Deleted unused CSS file related to icon styling.
    
    This refactor enhances maintainability and reduces bundle size.

--- packages/icons/src/components/icon-base.tsx
+++ packages/icons/src/components/icon-base.tsx
@@ -1,71 +1,31 @@
 import type { FunctionComponent, SVGProps } from 'react';
-import React from 'react';
-
-import clsx from 'clsx';
-
-import * as style from './icon-base.css';
 
 export interface IconBaseProps extends SVGProps<SVGSVGElement> {
-    width?: number | string;
-    height?: number | string;
-    size?: number | string;
+    width?: string | number;
+    height?: string | number;
+    size?: string | number;
     color?: string;
     className?: string;
     children: React.ReactNode;
 }
 
 export interface IconType extends FunctionComponent<Omit<IconBaseProps, 'children'>> {}
-type Size = number | string;
 
-const isCssVariable = (value?: Size) => typeof value === 'string' && value.startsWith('var(--');
-
-/**
- * Icon Component
- * @param children - children of the icon component
- * @param width - width of the icon. if size is provided, it will be ignored
- * @param height - height of the icon. if size is provided, it will be ignored
- * @param size - size of the icon. if width or height is provided, priority is given to size
- * @param className - className of the icon component
- * @param style - style of the icon component
- * @param color - color of the icon
- * @param props - props of the icon component
- * @returns icon component
- */
 function IconBase({
-    children,
-    width = 16,
-    height = 16,
-    size,
+    size = 16,
     className,
     style: customStyle,
+    children,
     color = 'currentColor',
     ...props
 }: IconBaseProps) {
-    const widthValue = size || width;
-    const heightValue = size || height;
     return (
         <svg
             aria-hidden="true"
+            className={className}
+            width={size}
+            height={size}
             fill={color}
-            /*
-                NOTE: 사용자가 dom attribute로 width, height를 지정할 수 있도록 함
-            */
-            {...(!isCssVariable(widthValue) && { width: widthValue })}
-            {...(!isCssVariable(heightValue) && { height: heightValue })}
-            className={clsx(
-                {
-                    [style.container_width]: isCssVariable(widthValue),
-                    [style.container_height]: isCssVariable(heightValue),
-                },
-                className,
-            )}
-            style={
-                {
-                    ...(isCssVariable(widthValue) && { '--vapor-icon-width': widthValue }),
-                    ...(isCssVariable(heightValue) && { '--vapor-icon-height': heightValue }),
-                    ...customStyle,
-                } as React.CSSProperties
-            }
             {...props}
         >
             {children}
--- packages/icons/tsup.config.ts
+++ packages/icons/tsup.config.ts
@@ -1,18 +1,4 @@
-import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
-import autoprefixer from 'autoprefixer';
-import postcss from 'postcss';
 import { defineConfig } from 'tsup';
-
-/**
- * @link https://vanilla-extract.style/documentation/integrations/esbuild/#processcss
- */
-async function processCss(css: string) {
-    const result = await postcss([autoprefixer]).process(css, {
-        from: undefined /* suppress source map warning */,
-    });
-
-    return result.css;
-}
 
 export default [
     // ESM, CJS
@@ -25,24 +11,6 @@ export default [
         treeshake: true,
         sourcemap: true,
         cjsInterop: true,
-        esbuildPlugins: [
-            vanillaExtractPlugin({
-                processCss,
-                identifiers: ({ hash, filePath, debugId }) => {
-                    /** debugId: human readable prefixes like variants, sprinkles properties, etc. */
-                    const componentName = filePath.split('/').pop()?.replace('.css.ts', '');
-
-                    return `${componentName}${debugId ? `-${debugId}` : ''}-${hash}`;
-                },
-            }),
-        ],
-        esbuildOptions(options, context) {
-            if (context.format === 'esm') {
-                options.banner = { js: 'import "./index.css";' };
-            } else {
-                options.banner = { js: 'require("./index.css");' };
-            }
-        },
     }),
 
     // TYPES
