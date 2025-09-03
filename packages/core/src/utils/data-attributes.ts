0880cf7d14c5d45634e576d6c652dfc3e44170ad
noah.choi <noah.choi@goorm.io>
2025-09-03 23:45:33 +0900

    feat: add new `Sheet` component (#187)

---
+++ packages/core/src/utils/data-attributes.ts
@@ -0,0 +1,27 @@
+type DataAttr = Record<string, string>;
+
+type SingleDataAttrValue = string | number | boolean | null | undefined;
+type SingleDataAttrReturn = DataAttr | Record<string, never>;
+
+export function createDataAttribute(key: string, value: SingleDataAttrValue): SingleDataAttrReturn {
+    if (value === null || value === undefined) {
+        return {};
+    }
+
+    const dataKey = `data-${key}`;
+
+    if (typeof value === 'boolean') {
+        return value ? { [dataKey]: '' } : {};
+    }
+
+    return { [dataKey]: String(value) };
+}
+
+type MultiDataAttrValue = Record<string, SingleDataAttrValue>;
+type MultiDataAttrReturn = DataAttr;
+
+export function createDataAttributes(attributes: MultiDataAttrValue): MultiDataAttrReturn {
+    return Object.entries(attributes).reduce((acc, [key, value]) => {
+        return { ...acc, ...createDataAttribute(key, value) };
+    }, {} as MultiDataAttrReturn);
+}
