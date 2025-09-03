e924edd6e213e76614b8afb28884a49cb4f5b5c2
noah.choi <noah.choi@goorm.io>
2025-09-03 15:55:15 +0900

    refactor: edit overlay interface

--- apps/website/src/components/demo/examples/menu-checkbox.tsx
+++ apps/website/src/components/demo/examples/menu-checkbox.tsx
@@ -12,25 +12,20 @@ export default function MenuCheckbox() {
     return (
         <Menu.Root>
             <Menu.Trigger render={<Button>보기 설정</Button>} />
-            <Menu.Portal>
-                <Menu.Content>
-                    <Menu.Group>
-                        <Menu.GroupLabel>보기 옵션</Menu.GroupLabel>
-                        <Menu.CheckboxItem checked={showToolbar} onCheckedChange={setShowToolbar}>
-                            툴바 표시
-                        </Menu.CheckboxItem>
-                        <Menu.CheckboxItem checked={showSidebar} onCheckedChange={setShowSidebar}>
-                            사이드바 표시
-                        </Menu.CheckboxItem>
-                        <Menu.CheckboxItem
-                            checked={showStatusbar}
-                            onCheckedChange={setShowStatusbar}
-                        >
-                            상태바 표시
-                        </Menu.CheckboxItem>
-                    </Menu.Group>
-                </Menu.Content>
-            </Menu.Portal>
+            <Menu.Content>
+                <Menu.Group>
+                    <Menu.GroupLabel>보기 옵션</Menu.GroupLabel>
+                    <Menu.CheckboxItem checked={showToolbar} onCheckedChange={setShowToolbar}>
+                        툴바 표시
+                    </Menu.CheckboxItem>
+                    <Menu.CheckboxItem checked={showSidebar} onCheckedChange={setShowSidebar}>
+                        사이드바 표시
+                    </Menu.CheckboxItem>
+                    <Menu.CheckboxItem checked={showStatusbar} onCheckedChange={setShowStatusbar}>
+                        상태바 표시
+                    </Menu.CheckboxItem>
+                </Menu.Group>
+            </Menu.Content>
         </Menu.Root>
     );
 }
