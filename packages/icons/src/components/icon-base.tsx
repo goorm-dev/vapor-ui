477c249c1699f829afd54c298059e6b08e59d824
ChanHyung-Lee <max.lee@goorm.io>
2025-09-10 11:27:06 +0900

    refactor(icons): remove unused CSS variables and dependencies from icon-base component

--- packages/icons/src/components/icon-base.tsx
+++ packages/icons/src/components/icon-base.tsx
@@ -1,14 +1,6 @@
 import type { FunctionComponent, SVGProps } from 'react';
 
-import { assignInlineVars } from '@vanilla-extract/dynamic';
-import clsx from 'clsx';
-
-import { vaporIconHeight, vaporIconWidth } from './icon-base.css';
-import * as styles from './icon-base.css';
-
 export interface IconBaseProps extends SVGProps<SVGSVGElement> {
-    width?: string | number;
-    height?: string | number;
     size?: string | number;
     color?: string;
     className?: string;
@@ -21,28 +13,17 @@ function IconBase({
     size = 16,
     width,
     height,
-    className,
     style,
     children,
     color = 'currentColor',
     ...props
 }: IconBaseProps) {
-    const iconWidth = width || size;
-    const iconHeight = height || size;
     return (
         <svg
             aria-hidden="true"
-            className={clsx(className, styles.container_width, styles.container_height)}
             width={size || width}
             height={size || height}
             fill={color}
-            style={{
-                ...assignInlineVars({
-                    [vaporIconHeight]: `${iconHeight.toString()}px`,
-                    [vaporIconWidth]: `${iconWidth.toString()}px`,
-                }),
-                ...style,
-            }}
             {...props}
         >
             {children}
