7deeea526e22984e401285f508fe3ecbe2faaa31
noah.choi <noah.choi@goorm.io>
2025-09-12 12:55:11 +0900

    chore: add select snapshots

--- packages/core/src/components/select/select.stories.tsx
+++ packages/core/src/components/select/select.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { Grid } from '../grid';
 import { Select } from './select';
 
 export default {
@@ -116,4 +117,120 @@ export const ArrayItems: StoryObj<typeof Select.Root> = {
             </Select.Content>
         </Select.Root>
     ),
+};
+
+export const TestBed = {
+    render: () => {
+        return (
+            <Grid.Root
+                gap="150px"
+                templateColumns="repeat(auto-fit, 150px)"
+                templateRows="repeat(auto-fit, minmax(150px, 1fr))"
+            >
+                <Grid.Item>
+                    <Select.Root>
+                        <Select.Trigger>
+                            <Select.DisplayValue placeholder="Placeholder" />
+                            <Select.TriggerIcon />
+                        </Select.Trigger>
+                    </Select.Root>
+                </Grid.Item>
+
+                <Grid.Item>
+                    <Select.Root defaultOpen>
+                        <Select.Trigger>
+                            <Select.DisplayValue placeholder="Grouped" />
+                            <Select.TriggerIcon />
+                        </Select.Trigger>
+                        <Select.Content>
+                            <Select.Group>
+                                <Select.GroupLabel>Font Group</Select.GroupLabel>
+                                {fonts.map((font) => (
+                                    <Select.Item key={font.value} value={font.value}>
+                                        {font.label}
+
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                ))}
+                            </Select.Group>
+                            <Select.Separator />
+                            <Select.Group>
+                                <Select.GroupLabel>Language Group</Select.GroupLabel>
+                                {Object.entries(languages).map(([value, label]) => (
+                                    <Select.Item key={value} value={value}>
+                                        {label}
+
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                ))}
+                            </Select.Group>
+                        </Select.Content>
+                    </Select.Root>
+                </Grid.Item>
+
+                <Grid.Item>
+                    <Select.Root defaultOpen defaultValue={'mono'}>
+                        <Select.Trigger>
+                            <Select.DisplayValue placeholder="Placeholder" />
+                            <Select.TriggerIcon />
+                        </Select.Trigger>
+                        <Select.Content>
+                            <Select.Group>
+                                <Select.GroupLabel>Font</Select.GroupLabel>
+                                {fonts.map((font) => (
+                                    <Select.Item key={font.value} value={font.value}>
+                                        {font.label}
+
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                ))}
+                            </Select.Group>
+                        </Select.Content>
+                    </Select.Root>
+                </Grid.Item>
+
+                <Grid.Item>
+                    <Select.Root defaultOpen items={languages} defaultValue={'csharp'}>
+                        <Select.Trigger>
+                            <Select.DisplayValue placeholder="Select Font" />
+                            <Select.TriggerIcon />
+                        </Select.Trigger>
+
+                        <Select.Content>
+                            <Select.Group>
+                                <Select.GroupLabel>Auto Label: </Select.GroupLabel>
+                                {Object.entries(languages).map(([value, label]) => (
+                                    <Select.Item key={value} value={value}>
+                                        {value} → {label}
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                ))}
+                            </Select.Group>
+                        </Select.Content>
+                    </Select.Root>
+                </Grid.Item>
+
+                <Grid.Item>
+                    <Select.Root items={fonts} defaultValue={'mono'} defaultOpen>
+                        <Select.Trigger>
+                            <Select.DisplayValue placeholder="Select Font" />
+                            <Select.TriggerIcon />
+                        </Select.Trigger>
+
+                        <Select.Content>
+                            <Select.Group>
+                                <Select.GroupLabel>Auto Label:</Select.GroupLabel>
+                                {fonts.map((font) => (
+                                    <Select.Item key={font.value} value={font.value}>
+                                        {font.value} → {font.label}
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                ))}
+                            </Select.Group>
+                        </Select.Content>
+                    </Select.Root>
+                </Grid.Item>
+            </Grid.Root>
+        );
+    },
 };
