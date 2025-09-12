9ce5b5fd5e4999eaa44641624966c9df758b3fd1
noah.choi <noah.choi@goorm.io>
2025-09-12 10:57:54 +0900

    feat: add placeholder prop

--- packages/core/src/components/select/select.stories.tsx
+++ packages/core/src/components/select/select.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { HStack } from '../h-stack';
 import { Select } from './select';
 
 export default {
@@ -17,43 +16,104 @@ export default {
     },
 } satisfies Meta<typeof Select.Root>;
 
+export const Default: StoryObj<typeof Select.Root> = {
+    render: (args) => (
+        <Select.Root {...args}>
+            <Select.Trigger>
+                <Select.DisplayValue />
+                <Select.TriggerIcon />
+            </Select.Trigger>
+
+            <Select.Content>
+                <Select.Group>
+                    <Select.GroupLabel>Font</Select.GroupLabel>
+                    <Select.Item value="sans">
+                        Sans-serif
+                        <Select.ItemIndicator />
+                    </Select.Item>
+                    <Select.Item value="serif">
+                        Serif
+                        <Select.ItemIndicator />
+                    </Select.Item>
+
+                    <Select.Item value="mono">
+                        Monospace
+                        <Select.ItemIndicator />
+                    </Select.Item>
+                    <Select.Item value="cursive">
+                        Cursive
+                        <Select.ItemIndicator />
+                    </Select.Item>
+                </Select.Group>
+            </Select.Content>
+        </Select.Root>
+    ),
+};
+
+const languages = {
+    javascript: 'JavaScript',
+    typescript: 'TypeScript',
+    python: 'Python',
+    java: 'Java',
+    csharp: 'C#',
+    php: 'PHP',
+    cpp: 'C++',
+    rust: 'Rust',
+    go: 'Go',
+    swift: 'Swift',
+};
+
+export const ObjectItems: StoryObj<typeof Select.Root> = {
+    render: (args) => (
+        <Select.Root items={languages} {...args}>
+            <Select.Trigger>
+                <Select.DisplayValue placeholder="Select Font" />
+                <Select.TriggerIcon />
+            </Select.Trigger>
+
+            <Select.Content>
+                <Select.Group>
+                    <Select.GroupLabel>Font</Select.GroupLabel>
+                    {Object.entries(languages).map(([value, label]) => (
+                        <Select.Item key={value} value={value}>
+                            {label}
+
+                            <Select.ItemIndicator />
+                        </Select.Item>
+                    ))}
+                </Select.Group>
+            </Select.Content>
+        </Select.Root>
+    ),
+};
+
 const fonts = [
-    { label: 'Select Item', value: null },
     { label: 'Sans-serif', value: 'sans' },
     { label: 'Serif', value: 'serif' },
     { label: 'Monospace', value: 'mono' },
     { label: 'Cursive', value: 'cursive' },
 ];
 
-export const Default: StoryObj<typeof Select.Root> = {
+export const ArrayItems: StoryObj<typeof Select.Root> = {
     render: (args) => (
-        <HStack>
-            <Select.Root {...args} items={fonts}>
-                <Select.Trigger>
-                    <Select.DisplayValue />
-                    <Select.TriggerIcon />
-                </Select.Trigger>
+        <Select.Root items={fonts} {...args}>
+            <Select.Trigger>
+                <Select.DisplayValue placeholder="Select Font" />
+                <Select.TriggerIcon />
+            </Select.Trigger>
 
-                <Select.Content>
-                    <Select.Group>
-                        <Select.GroupLabel>Font</Select.GroupLabel>
-                        {fonts.map((font) => (
-                            <Select.Item key={font.value} value={font.value}>
-                                {font.label}
+            <Select.Content>
+                <Select.Group>
+                    <Select.GroupLabel>Font</Select.GroupLabel>
+                    {fonts.map((font) => (
+                        <Select.Item key={font.value} value={font.value}>
+                            {font.label}
 
-                                <Select.ItemIndicator />
-                            </Select.Item>
-                        ))}
-                    </Select.Group>
-                </Select.Content>
-            </Select.Root>
-            <Select.Root>
-                <Select.Trigger>
-                    <Select.Placeholder>sans</Select.Placeholder>
-
-                    <Select.TriggerIcon />
-                </Select.Trigger>
-            </Select.Root>
-        </HStack>
+                            <Select.ItemIndicator />
+                        </Select.Item>
+                    ))}
+                </Select.Group>
+            </Select.Content>
+        </Select.Root>
     ),
 };
