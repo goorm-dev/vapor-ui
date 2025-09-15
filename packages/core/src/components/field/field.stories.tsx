d2b3796f24c8b3504e4db63bf6bb3c1f3d39be00
ChanHyung-Lee <max.lee@goorm.io>
2025-09-15 13:30:24 +0900

    feat(Field): update TestBed stories with new labels and descriptions for enhanced user feedback

--- packages/core/src/components/field/field.stories.tsx
+++ packages/core/src/components/field/field.stories.tsx
@@ -9,6 +9,8 @@ import { RadioGroup } from '~/components/radio-group';
 import { Switch } from '~/components/switch';
 import { TextInput } from '~/components/text-input';
 
+import { Text } from '../text';
+
 type FieldStoryArgs = React.ComponentProps<typeof Field.Root> & {
     required?: boolean;
 };
@@ -41,40 +43,71 @@ export const TestBed: Story = {
         return (
             <Flex flexDirection="column" gap="$300">
                 <Field.Root name="vapor-policy-agreement" validationMode="onChange" {...fieldArgs}>
+                    <Text typography="heading5" render={<legend />}>
+                        좋았던 강의는 무엇인가요?
+                    </Text>
+                    <Field.Description>중복 선택 가능</Field.Description>
                     <Flex gap="$100">
                         <Checkbox.Root required={required}>
                             <Checkbox.Indicator>
                                 <ConfirmOutlineIcon />
                             </Checkbox.Indicator>
                         </Checkbox.Root>
-                        <Field.Label>동의합니다.</Field.Label>
+                        <Field.Label>멘토님 강연 능력</Field.Label>
                     </Flex>
-                    <Field.Description>
-                        I agree to the Terms and Conditions and Privacy Policy
-                    </Field.Description>
-                    <Field.Error>
-                        You must agree to the terms and conditions to continue
-                    </Field.Error>
-                    <Field.Success>
-                        ✓ Thank you for accepting our terms and conditions
-                    </Field.Success>
+                    <Flex gap="$100">
+                        <Checkbox.Root required={required}>
+                            <Checkbox.Indicator>
+                                <ConfirmOutlineIcon />
+                            </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <Field.Label>주제(협업 및 커뮤니케이션 스킬)</Field.Label>
+                    </Flex>
+                    <Flex gap="$100">
+                        <Checkbox.Root required={required}>
+                            <Checkbox.Indicator>
+                                <ConfirmOutlineIcon />
+                            </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <Field.Label>전반적인 강의 내용</Field.Label>
+                    </Flex>
+                    <Flex gap="$100">
+                        <Checkbox.Root required={required}>
+                            <Checkbox.Indicator>
+                                <ConfirmOutlineIcon />
+                            </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <Field.Label>세미나 자료</Field.Label>
+                    </Flex>
+                    <Flex gap="$100">
+                        <Checkbox.Root required={required}>
+                            <Checkbox.Indicator>
+                                <ConfirmOutlineIcon />
+                            </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <Field.Label>기타</Field.Label>
+                    </Flex>
+                    <Field.Error>좋았던 강의를 최소 하나 이상 선택해주세요</Field.Error>
+                    <Field.Success>✓ 강의 평가가 완료되었습니다</Field.Success>
                 </Field.Root>
                 {/* Switch Component Example */}
                 <Field.Root name="notifications" validationMode="onChange" {...fieldArgs}>
-                    <Flex alignItems="center" gap="$150">
+                    <Text typography="heading5" render={<legend />}>
+                        개인 정보 수신 동의
+                    </Text>
+                    <Flex alignItems="center" gap="$100" justifyContent="space-between">
+                        <Field.Label>서비스 메일 수신 동의</Field.Label>
                         <Switch.Root required={required} />
-                        <Field.Label>Enable push notifications</Field.Label>
+                    </Flex>
+                    <Flex alignItems="center" gap="$100" justifyContent="space-between">
+                        <Field.Label>이벤트성 광고 수신 동의</Field.Label>
+                        <Switch.Root required={required} />
                     </Flex>
                     <Field.Description>
-                        Get notified about important updates, security alerts, and account
-                        activities
+                        서비스 관련 메일과 이벤트 정보를 받아보실 수 있습니다
                     </Field.Description>
-                    <Field.Error>
-                        Please enable notifications to receive important updates
-                    </Field.Error>
-                    <Field.Success>
-                        ✓ Notifications enabled - you'll receive important updates
-                    </Field.Success>
+                    <Field.Error>개인 정보 수신 동의가 필요합니다</Field.Error>
+                    <Field.Success>✓ 개인 정보 수신 동의가 완료되었습니다</Field.Success>
                 </Field.Root>
 
                 {/* RadioGroup Selection RadioGroup */}
