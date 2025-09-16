df9020ee2af61924a5a4f96e48c5b24d317d44a9
ZeroChoi2781 <zero.choi@goorm.io>
2025-09-16 21:15:16 +0900

    feat: figma-plugin 워크스페이스 생성 및 vite 세팅

---
+++ apps/figma-plugin/vite.config.ui.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(), 
+    tailwindcss(), 
+    ...(mode === 'production' ? [viteSingleFile()] : [])
+  ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    minify: mode === "production",
+    cssMinify: mode === "production",
+    sourcemap: mode !== "production" ? "inline" : false,
+    emptyOutDir: false,
+    outDir: path.resolve("dist"),
+    rollupOptions: {
+      input: path.resolve("index.html"),
+    },
+  },
+}));
