7928a67dd17cc5980d551c603db980c35e2f736f
noah.choi <noah.choi@goorm.io>
2025-09-21 23:20:03 +0900

    refactor: add subcomponent props to `Content` (#247)

--- apps/website/src/components/demo/examples/dialog-size.tsx
+++ apps/website/src/components/demo/examples/dialog-size.tsx
@@ -5,7 +5,7 @@ export default function DialogSize() {
         <div className="space-x-4">
             <Dialog.Root size="md">
                 <Dialog.Trigger render={<Button>Medium Dialog</Button>} />
-                <Dialog.CombinedContent>
+                <Dialog.Content>
                     <Dialog.Header>
                         <Dialog.Title>Medium Size</Dialog.Title>
                     </Dialog.Header>
@@ -16,12 +16,12 @@ export default function DialogSize() {
                         <Dialog.Close render={<Button variant="ghost">취소</Button>} />
                         <Button>확인</Button>
                     </Dialog.Footer>
-                </Dialog.CombinedContent>
+                </Dialog.Content>
             </Dialog.Root>
 
             <Dialog.Root size="lg">
                 <Dialog.Trigger render={<Button>Large Dialog</Button>} />
-                <Dialog.CombinedContent>
+                <Dialog.Content>
                     <Dialog.Header>
                         <Dialog.Title>Large Size</Dialog.Title>
                     </Dialog.Header>
@@ -32,12 +32,12 @@ export default function DialogSize() {
                         <Dialog.Close render={<Button variant="ghost">취소</Button>} />
                         <Button>확인</Button>
                     </Dialog.Footer>
-                </Dialog.CombinedContent>
+                </Dialog.Content>
             </Dialog.Root>
 
             <Dialog.Root size="xl">
                 <Dialog.Trigger render={<Button>Extra Large Dialog</Button>} />
-                <Dialog.CombinedContent>
+                <Dialog.Content>
                     <Dialog.Header>
                         <Dialog.Title>Extra Large Size</Dialog.Title>
                     </Dialog.Header>
@@ -50,7 +50,7 @@ export default function DialogSize() {
                         <Dialog.Close render={<Button variant="ghost">취소</Button>} />
                         <Button>확인</Button>
                     </Dialog.Footer>
-                </Dialog.CombinedContent>
+                </Dialog.Content>
             </Dialog.Root>
         </div>
     );
