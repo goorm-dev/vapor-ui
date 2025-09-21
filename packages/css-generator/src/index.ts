5a4c702a44b5ac8f35dc15b7e434e0c8fb9bf8c1
ZeroChoi2781 <zero.choi@goorm.io>
2025-09-21 20:09:34 +0900

    chore: re-export 제거

--- packages/css-generator/src/index.ts
+++ packages/css-generator/src/index.ts
@@ -1,14 +1,4 @@
-// Main exports
 export * from './generators';
 export * from './composers';
 export * from './types';
 export * from './utils';
-
-// Re-export types for convenience
-export type {
-    ColorThemeConfig,
-    CompleteCSSConfig,
-    ThemeClassNames,
-    CSSGeneratorOptions,
-    ThemeVariant,
-} from './types';
