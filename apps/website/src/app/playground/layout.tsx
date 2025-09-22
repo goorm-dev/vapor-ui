e92b63b7ef6825959dc255743d5168ffee71ca65
ZeroChoi2781 <zero.choi@goorm.io>
2025-09-22 17:43:48 +0900

    feat: ThemeScript 로직 제거

--- apps/website/src/app/playground/layout.tsx
+++ apps/website/src/app/playground/layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from 'react';
 
-import { ThemeProvider, ThemeScript, createThemeConfig } from '@vapor-ui/core';
 import type { Metadata } from 'next';
 
 import { SiteNavBar } from '~/components/site-nav-bar/site-nav-bar';
@@ -9,20 +8,11 @@ export const metadata: Metadata = {
     title: 'Playground - Vapor UI',
 };
 
-const themeConfig = createThemeConfig({
-    appearance: 'light',
-    radius: 'md',
-    scaling: 1,
-});
-
 export default function Layout({ children }: { children: ReactNode }) {
     return (
         <>
-            <ThemeScript config={themeConfig} />
-            <ThemeProvider config={themeConfig}>
-                <SiteNavBar />
-                {children}
-            </ThemeProvider>
+            <SiteNavBar />
+            {children}
         </>
     );
 }
