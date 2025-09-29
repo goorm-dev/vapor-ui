94c862f0fcc2c65664c622e15aaecf80550af1cc
ChanHyung-Lee <max.lee@goorm.io>
2025-09-29 11:16:56 +0900

    refactor: Simplify BadgeProps interface by removing redundant prop definitions

--- packages/core/src/components/badge/badge.tsx
+++ packages/core/src/components/badge/badge.tsx
@@ -10,29 +10,13 @@ import type { BadgeVariants } from './badge.css';
 import * as styles from './badge.css';
 
 type BadgePrimitiveProps = VComponentProps<'span'>;
-interface BadgeProps extends BadgePrimitiveProps, BadgeVariants {
-    /**
-     * ko: Badge의 색상을 설정합니다.
-     * en: Sets the color scheme of the Badge.
-     */
-    color?: BadgeVariants['color'];
-
-    /**
-     * ko: Badge의 크기를 설정합니다.
-     * en: Sets the size of the Badge.
-     */
-    size?: BadgeVariants['size'];
-
-    /**
-     * ko: Badge의 모양을 설정합니다.
-     * en: Sets the shape of the Badge.
-     */
-    shape?: BadgeVariants['shape'];
-}
+interface BadgeProps extends BadgePrimitiveProps, BadgeVariants {}
 
 /**
- * ko: Badge는 이미지, 컨텐츠 등의 상태 또는 분류를 시각적으로 표시합니다.
- * en: english description
+ * 이미지, 콘텐츠 등의 상태 또는 분류를 시각적으로 표시합니다.
+ * `span` 태그를 기본으로 사용합니다.
+ *
+ * {@see https://vapor-ui.goorm.io/docs/components/badge Badge Documentation}
  */
 
 const Badge = forwardRef<HTMLSpanElement, BadgeProps>(({ render, className, ...props }, ref) => {
