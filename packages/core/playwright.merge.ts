1b09eb788f2c002fb83552adcf9cc585f004f0d2
noah.choi <noah.choi@goorm.io>
2025-09-30 10:31:20 +0900

    json reporter

--- packages/core/playwright.merge.ts
+++ packages/core/playwright.merge.ts
@@ -1,3 +1,6 @@
 export default {
-    reporter: [['html', { outputFolder: './__tests__/report' }]],
+    reporter: [
+        ['html', { outputFolder: './__tests__/report' }],
+        ['json', { outputFile: './__tests__/report/index.json' }],
+    ],
 };
