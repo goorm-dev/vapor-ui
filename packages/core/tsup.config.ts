f0f643bb1d21baba8096535855751a53c22d27ac
zero.choi <zero.choi@goorm.io>
2025-10-02 10:02:43 +0900

    feat: Remove Font Loading from CSS Build (#265)

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -138,9 +138,6 @@ export default defineConfig([
             tailwind: 'src/styles/tailwind-preset.css.ts',
         },
         outDir: DIST_DIR,
-        banner: {
-            css: '@import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css");'
-        },
         esbuildPlugins: [
             vanillaExtractPlugin({
                 ...vanillaExtractConfig,
