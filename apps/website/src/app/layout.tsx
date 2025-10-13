636e140b6c74aa66fe563418df7897c244a46c02
ZeroChoi2781 <zero.choi@goorm.io>
2025-10-13 21:02:14 +0900

    docs(layout.tsx): theme-provider 사용법 변경

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -2,7 +2,7 @@ import './global.css';
 
 import type { ReactNode } from 'react';
 
-import { ThemeProvider } from '@vapor-ui/core';
+import { ThemeProvider } from '@vapor-ui/core/theme-provider';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
@@ -32,16 +32,16 @@ export default function Layout({ children }: { children: ReactNode }) {
                 />
             </head>
             <body className="flex flex-col min-h-screen bg-[var(--vapor-color-background-canvas)]">
-                <RootProvider
-                    search={{
-                        SearchDialog: DefaultSearchDialog,
-                    }}
-                    theme={{ enabled: false }}
-                >
-                    <ThemeProvider defaultTheme="system" enableSystem>
+                <ThemeProvider defaultTheme="system" enableSystem storageKey="vapor-docs-default">
+                    <RootProvider
+                        search={{
+                            SearchDialog: DefaultSearchDialog,
+                        }}
+                        theme={{ enabled: false }}
+                    >
                         {children}
-                    </ThemeProvider>
-                </RootProvider>
+                    </RootProvider>
+                </ThemeProvider>
             </body>
         </html>
     );
