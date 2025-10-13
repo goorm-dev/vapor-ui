ddcd9f308f72abfbae646d326620e889eb14c930
Yunsup Sim <dev@ethansup.net>
2025-10-13 02:33:46 +0000

    refactor: Change syncFigmaIcons.js to ESM

--- scripts/apis/figma.js
+++ scripts/apis/figma.js
@@ -1,7 +1,4 @@
-import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import process from 'node:process';
 
 const headers = {
     'X-FIGMA-TOKEN': process.env.FIGMA_TOKEN,
@@ -13,7 +10,7 @@ const headers = {
  * @link https://www.figma.com/developers/api#get-file-nodes-endpoint
  */
 const getFileNodes = async ({ fileKey, nodeIds, depth = 1 }) => {
-    const { data } = await axios.get(
+    const { data } = await fetch(
         `https://api.figma.com/v1/files/${fileKey}/nodes?ids=${nodeIds}&depth=${depth}`,
         { headers },
     );
@@ -26,7 +23,7 @@ const getFileNodes = async ({ fileKey, nodeIds, depth = 1 }) => {
  * @link https://www.figma.com/developers/api#get-images-endpoint
  */
 const getImage = async ({ fileKey, nodeIds, format = 'svg' }) => {
-    const { data } = await axios.get(
+    const { data } = await fetch(
         `https://api.figma.com/v1/images/${fileKey}?ids=${nodeIds}&format=${format}&svg_include_id=false`,
         {
             headers,
--- scripts/libs/figma.js
+++ scripts/libs/figma.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 import { getFileNodes, getImage } from '../apis/figma.js';
 import { makeFlexibleColorIcon, remakeMaskStyle, svgToIconComponent } from '../utils/figma.js';
 
@@ -53,7 +51,7 @@ const getNodesWithUrl = async ({ nodes, fileKey }) => {
  * Convert svg files from Figma to React components.
  */
 const getIconJsx = async ({ url, isColorIcon }) => {
-    const { data: svgDom } = await axios.get(url);
+    const { data: svgDom } = await fetch(url);
     const IconComponent = svgToIconComponent(svgDom);
     const NewIconComponent = remakeMaskStyle(IconComponent);
 
