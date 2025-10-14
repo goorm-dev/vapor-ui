fc5fe776b311461b9980cae993380140fa599146
Yunsup Sim <dev@ethansup.net>
2025-10-14 14:33:24 +0900

    fix: Fix fetch code

--- scripts/apis/figma.js
+++ scripts/apis/figma.js
@@ -10,11 +10,11 @@ const headers = {
  * @link https://www.figma.com/developers/api#get-file-nodes-endpoint
  */
 const getFileNodes = async ({ fileKey, nodeIds, depth = 1 }) => {
-    const { data } = await fetch(
+    const result = await fetch(
         `https://api.figma.com/v1/files/${fileKey}/nodes?ids=${nodeIds}&depth=${depth}`,
         { headers },
     );
-    return data;
+    return result.json();
 };
 
 /**
@@ -23,14 +23,14 @@ const getFileNodes = async ({ fileKey, nodeIds, depth = 1 }) => {
  * @link https://www.figma.com/developers/api#get-images-endpoint
  */
 const getImage = async ({ fileKey, nodeIds, format = 'svg' }) => {
-    const { data } = await fetch(
+    const result = await fetch(
         `https://api.figma.com/v1/images/${fileKey}?ids=${nodeIds}&format=${format}&svg_include_id=false`,
         {
             headers,
         },
     );
 
-    return data;
+    return result.json();
 };
 
 export { getFileNodes, getImage };
