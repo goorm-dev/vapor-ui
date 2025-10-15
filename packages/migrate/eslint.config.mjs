64c68ef47bac6e4cdb2a9043678564be130668d7
ChanHyung-Lee <max.lee@goorm.io>
2025-10-15 10:46:38 +0900

    feat: Update ESLint configuration and add eslint as a dependency

--- packages/migrate/eslint.config.mjs
+++ packages/migrate/eslint.config.mjs
@@ -1,3 +1,14 @@
 import { configs } from '@repo/eslint-config/base';
 
-export default [...configs];
+export default [
+    ...configs,
+    {
+        excludedFiles: [
+            '**/dist/**',
+            '**/node_modules/**',
+            '**/build/**',
+            '**/coverage/**',
+            '**/__textfixtures__/**',
+        ],
+    },
+];
