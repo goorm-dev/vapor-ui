88a21c3cd513072518f703179614d631058add3d
noah.choi <noah.choi@goorm.io>
2025-10-21 14:12:37 +0900

    refactor: split storybook package (#268)

--- apps/storybook/eslint.config.js
+++ apps/storybook/eslint.config.js
@@ -1,6 +1,5 @@
 // @ts-check
 import { configs as reactPackage } from '@repo/eslint-config/react-package';
 import { configs as storybook } from '@repo/eslint-config/storybook';
-import { configs as vanillaExtract } from '@repo/eslint-config/vanilla-extract';
 
-export default [...reactPackage, ...vanillaExtract, ...storybook];
+export default [...reactPackage, ...storybook];
--- packages/core/src/components/text/text.stories.tsx
+++ packages/core/src/components/text/text.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { VStack } from '../v-stack';
 import { Text } from './text';
