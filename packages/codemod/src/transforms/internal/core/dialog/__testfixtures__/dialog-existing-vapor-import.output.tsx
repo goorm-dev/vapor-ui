c139da48d073478cacc3382e482f0ba98beaca8b
ChanHyung-Lee <max.lee@goorm.io>
2025-10-31 00:23:16 +0900

    refactor(codemod): remove obsolete test fixtures for Collapsible and Dialog components migration to @vapor-ui/core

--- packages/codemod/src/transforms/internal/core/dialog/__testfixtures__/dialog-existing-vapor-import.output.tsx
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { Button, Dialog } from '@vapor-ui/core';
-
-export const Component = () => (
-    <Dialog.Root>
-        <Dialog.Trigger render={<Button>Open</Button>} />
-        <Dialog.Content>Content</Dialog.Content>
-    </Dialog.Root>
-);
