7d4540787f41a37632d35ca2bca139917d604e83
ChanHyung-Lee <max.lee@goorm.io>
2025-10-30 17:06:43 +0900

    refactor(codemod): remove obsolete Alert test fixtures and migrate to Callout component

--- packages/codemod/src/transforms/internal/core/alert/__testfixtures__/alert-with-expression.output.tsx
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-import { Callout } from '@vapor-ui/core';
-
-const color = 'success';
-
-export const Component = () => <Callout.Root color={color as any}>Dynamic color</Callout.Root>;
