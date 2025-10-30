52234e51344ddee688b3f8ec1a2d8773936d405f
ZeroChoi2781 <zero.choi@goorm.io>
2025-10-30 19:50:19 +0900

    chore: formatted

--- apps/website/src/components/demo/examples/radio-card-readonly.tsx
+++ apps/website/src/components/demo/examples/radio-card-readonly.tsx
@@ -5,20 +5,20 @@ export default function RadioCardReadonly() {
         <div className="space-y-6">
             <RadioGroup.Root name="readonly-group" readOnly defaultValue="selected">
                 <RadioGroup.Label>읽기 전용 상태</RadioGroup.Label>
-                    <HStack gap="$100">
-                        <RadioCard value="unselected">Unselected Option</RadioCard>
-                        <RadioCard value="selected">Selected Option (Read Only)</RadioCard>
-                        <RadioCard value="another">Another Option</RadioCard>
-                    </HStack>
+                <HStack gap="$100">
+                    <RadioCard value="unselected">Unselected Option</RadioCard>
+                    <RadioCard value="selected">Selected Option (Read Only)</RadioCard>
+                    <RadioCard value="another">Another Option</RadioCard>
+                </HStack>
             </RadioGroup.Root>
 
             <RadioGroup.Root name="normal-group" defaultValue="selected-normal">
                 <RadioGroup.Label>일반 상태 (비교용)</RadioGroup.Label>
-                    <HStack gap="$100">
-                        <RadioCard value="unselected-normal">Unselected Option</RadioCard>
-                        <RadioCard value="selected-normal">Selected Option (Editable)</RadioCard>
-                        <RadioCard value="another-normal">Another Option</RadioCard>
-                    </HStack>
+                <HStack gap="$100">
+                    <RadioCard value="unselected-normal">Unselected Option</RadioCard>
+                    <RadioCard value="selected-normal">Selected Option (Editable)</RadioCard>
+                    <RadioCard value="another-normal">Another Option</RadioCard>
+                </HStack>
             </RadioGroup.Root>
         </div>
     );
