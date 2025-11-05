5f77ccd4aea47690bf20260be050ce06d4c253e0
noah.choi <noah.choi@goorm.io>
2025-11-05 14:42:23 +0900

    icon-button resolve styles

--- packages/core/src/components/icon-button/icon-button.tsx
+++ packages/core/src/components/icon-button/icon-button.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 
 import { createSlot } from '~/libs/create-slot';
 import { createSplitProps } from '~/utils/create-split-props';
+import { resolveStyles } from '~/utils/resolve-styles';
 import type { VComponentProps } from '~/utils/types';
 
 import { Button } from '../button';
@@ -11,8 +12,18 @@ import type { IconButtonVariants } from './icon-button.css';
 import * as styles from './icon-button.css';
 
 export const IconButton = forwardRef<HTMLButtonElement, IconButton.Props>((props, ref) => {
-    const { 'aria-label': ariaLabel, className, children, ...componentProps } = props;
-    const [variantProps, otherProps] = createSplitProps<IconButtonVariants>()(props, ['shape']);
+    const {
+        'aria-label': ariaLabel,
+        className,
+        children,
+        ...componentProps
+    } = resolveStyles(props);
+
+    const [variantProps, otherProps] = createSplitProps<IconButtonVariants>()(componentProps, [
+        'shape',
+    ]);
+
+    const { size } = otherProps;
 
     const IconElement = createSlot(children);
 
@@ -21,10 +32,10 @@ export const IconButton = forwardRef<HTMLButtonElement, IconButton.Props>((props
             ref={ref}
             aria-label={ariaLabel}
             className={clsx(styles.root(variantProps), className)}
-            {...componentProps}
+            {...otherProps}
             stretch={false}
         >
-            <IconElement aria-hidden className={styles.icon({ size: otherProps.size })} />
+            <IconElement aria-hidden className={styles.icon({ size })} />
         </Button>
     );
 });
