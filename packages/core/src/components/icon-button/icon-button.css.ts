7d1d16485e65bc7cdbbac11ae65f0fec944e4cd6
ZeroChoi2781 <zero.choi@goorm.io>
2025-11-05 20:58:28 +0900

    chore: icon-button에서 button.css 불러오는 로직 제거

--- packages/core/src/components/icon-button/icon-button.css.ts
+++ packages/core/src/components/icon-button/icon-button.css.ts
@@ -4,11 +4,8 @@ import { recipe } from '@vanilla-extract/recipes';
 import { layerStyle } from '~/styles/mixins/layer-style.css';
 import { vars } from '~/styles/themes.css';
 
-import { root as buttonRecipe } from '../button/button.css';
-
 export const root = recipe({
     base: [
-        buttonRecipe.classNames.base,
         layerStyle('components', {
             aspectRatio: '1 / 1',
             padding: 0,
