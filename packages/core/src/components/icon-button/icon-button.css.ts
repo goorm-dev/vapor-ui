f171df9c447a932860d4508e8edebc94e16ee9f7
noah.choi <noah.choi@goorm.io>
2025-11-17 14:57:17 +0900

    prevent to trigger event from svg of icon-button

--- packages/core/src/components/icon-button/icon-button.css.ts
+++ packages/core/src/components/icon-button/icon-button.css.ts
@@ -22,6 +22,7 @@ export const root = recipe({
 });
 
 export const icon = recipe({
+    base: layerStyle('components', { pointerEvents: 'none' }),
     variants: {
         size: {
             sm: layerStyle('components', {
