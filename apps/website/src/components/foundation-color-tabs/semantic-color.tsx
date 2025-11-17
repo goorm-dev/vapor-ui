77f247eb6d9f80b652f129d78554d783d978588b
noah.choi <noah.choi@goorm.io>
2025-11-17 13:29:48 +0900

    remove legacy tokens

--- apps/website/src/components/foundation-color-tabs/semantic-color.tsx
+++ apps/website/src/components/foundation-color-tabs/semantic-color.tsx
@@ -13,50 +13,58 @@ export const SemanticColor = () => {
                     {
                         name: 'Background Primary 200',
                         variable: 'background-primary-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     { name: 'Background Secondary 100', variable: 'background-secondary-100' },
                     {
                         name: 'Background Secondary 200',
                         variable: 'background-secondary-200',
-                        foreground: 'gray-950',
+                        foreground: 'gray-900',
                     },
                     { name: 'Background Success 100', variable: 'background-success-100' },
                     {
                         name: 'Background Success 200',
                         variable: 'background-success-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     { name: 'Background Warning 100', variable: 'background-warning-100' },
                     {
                         name: 'Background Warning 200',
                         variable: 'background-warning-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     { name: 'Background Danger 100', variable: 'background-danger-100' },
                     {
                         name: 'Background Danger 200',
                         variable: 'background-danger-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     { name: 'Background Hint 100', variable: 'background-hint-100' },
                     {
                         name: 'Background Hint 200',
                         variable: 'background-hint-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     { name: 'Background Contrast 100', variable: 'background-contrast-100' },
                     {
                         name: 'Background Contrast 200',
                         variable: 'background-contrast-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
-                    { name: 'Background Canvas', variable: 'background-canvas' },
-                    { name: 'Background Surface 100', variable: 'background-surface-100' },
                     {
-                        name: 'Background Surface 200',
-                        variable: 'background-surface-200',
-                        foreground: 'gray-950',
+                        name: 'Background Canvas 100',
+                        variable: 'background-canvas-100',
+                        foreground: 'gray-900',
+                    },
+                    {
+                        name: 'Background Canvas 200',
+                        variable: 'background-canvas-200',
+                        foreground: 'gray-900',
+                    },
+                    {
+                        name: 'Background Overlay 100',
+                        variable: 'background-overlay-100',
+                        foreground: 'gray-900',
                     },
                 ]}
             />
@@ -68,82 +76,82 @@ export const SemanticColor = () => {
                     {
                         name: 'Foreground Primary 100',
                         variable: 'foreground-primary-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Primary 200',
                         variable: 'foreground-primary-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Secondary 100',
                         variable: 'foreground-secondary-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Secondary 200',
                         variable: 'foreground-secondary-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Success 100',
                         variable: 'foreground-success-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Success 200',
                         variable: 'foreground-success-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Warning 100',
                         variable: 'foreground-warning-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Warning 200',
                         variable: 'foreground-warning-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Danger 100',
                         variable: 'foreground-danger-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Danger 200',
                         variable: 'foreground-danger-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Hint 100',
                         variable: 'foreground-hint-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Hint 200',
                         variable: 'foreground-hint-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Contrast 100',
                         variable: 'foreground-contrast-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Contrast 200',
                         variable: 'foreground-contrast-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Normal 100',
                         variable: 'foreground-normal-100',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                     {
                         name: 'Foreground Normal 200',
                         variable: 'foreground-normal-200',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
                 ]}
             />
@@ -152,22 +160,22 @@ export const SemanticColor = () => {
 
             <ColorPalette
                 colors={[
-                    { name: 'Border Primary', variable: 'border-primary', foreground: 'gray-000' },
+                    { name: 'Border Primary', variable: 'border-primary', foreground: 'gray-050' },
                     {
                         name: 'Border Secondary',
                         variable: 'border-secondary',
-                        foreground: 'gray-950',
+                        foreground: 'gray-900',
                     },
-                    { name: 'Border Success', variable: 'border-success', foreground: 'gray-000' },
-                    { name: 'Border Warning', variable: 'border-warning', foreground: 'gray-000' },
-                    { name: 'Border Danger', variable: 'border-danger', foreground: 'gray-000' },
-                    { name: 'Border Hint', variable: 'border-hint', foreground: 'gray-000' },
+                    { name: 'Border Success', variable: 'border-success', foreground: 'gray-050' },
+                    { name: 'Border Warning', variable: 'border-warning', foreground: 'gray-050' },
+                    { name: 'Border Danger', variable: 'border-danger', foreground: 'gray-050' },
+                    { name: 'Border Hint', variable: 'border-hint', foreground: 'gray-050' },
                     {
                         name: 'Border Contrast',
                         variable: 'border-contrast',
-                        foreground: 'gray-000',
+                        foreground: 'gray-050',
                     },
-                    { name: 'Border Normal', variable: 'border-normal', foreground: 'gray-950' },
+                    { name: 'Border Normal', variable: 'border-normal', foreground: 'gray-900' },
                 ]}
             />
 
