eb3fdc7862986e39f9d7d925680873083b346722
MaxLee <126851396+MaxLee-dev@users.noreply.github.com>
2025-11-18 13:32:59 +0900

    feat(codemod): add Alert, Badge, Button component transforms (#314)

---
+++ packages/codemod/src/transforms/internal/core/button/__testfixtures__/button-basic.output.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@vapor-ui/core';
+
+export const Component = () => (
+    <>
+        <Button variant="fill">Primary</Button>
+        <Button variant="outline">Secondary</Button>
+        <Button variant="ghost">Tertiary</Button>
+    </>
+);
