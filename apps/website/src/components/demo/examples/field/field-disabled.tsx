40bfa0a6b673070751942fbc867f5e525aa74957
noah.choi <noah.choi@goorm.io>
2025-11-28 15:59:37 +0900

    docs: website tone review (#360)

--- apps/website/src/components/demo/examples/field/field-disabled.tsx
+++ apps/website/src/components/demo/examples/field/field-disabled.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import { Box, Field, TextInput } from '@vapor-ui/core';
+import { Field, Text, TextInput } from '@vapor-ui/core';
 
 export default function FieldDisabled() {
     return (
         <Field.Root name="disabled" disabled>
-            <Box render={<Field.Label />} flexDirection="column">
-                비활성 필드
+            <Field.Label flexDirection="column">
+                <Text typography="subtitle2" foreground="secondary-100">
+                    비활성 필드
+                </Text>
                 <TextInput value="이 필드는 수정할 수 없습니다" />
-            </Box>
+            </Field.Label>
 
             <Field.Description>이 필드는 현재 비활성화되어 수정할 수 없습니다.</Field.Description>
         </Field.Root>
