bd293c64d77ab872474d275a1d5fdeadf691bc3f
noah.choi <noah.choi@goorm.io>
2026-01-06 17:12:19 +0900

    add comments

--- packages/eslint-plugin-vapor/src/types/estree.d.ts
+++ packages/eslint-plugin-vapor/src/types/estree.d.ts
@@ -1,5 +1,12 @@
 import 'estree-jsx';
 
+/**
+ * This file augments the `estree-jsx` types to include types that are not built-in to `estree-jsx` but are part of the JSX AST spec.
+ * These types are necessary for writing ESLint rules that interact with JSX AST nodes.
+ *
+ * References:
+ * - https://github.com/facebook/jsx/blob/main/AST.md
+ */
 declare module 'estree-jsx' {
     interface JSXMemberExpression {
         type: 'JSXMemberExpression';
