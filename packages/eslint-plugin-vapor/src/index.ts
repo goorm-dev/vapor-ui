3a25a5a26f336250b8cda80f288647942a8eda54
noah.choi <noah.choi@goorm.io>
2026-01-06 15:47:19 +0900

    add aria-label-on-dialog

--- packages/eslint-plugin-vapor/src/index.ts
+++ packages/eslint-plugin-vapor/src/index.ts
@@ -1,9 +1,11 @@
 import type { Rule } from 'eslint';
 
+import { ariaLabelOnDialogRule } from './rules/aria-label-on-dialog';
 import { ariaLabelOnIconButtonRule } from './rules/aria-label-on-icon-button';
 import { ariaLabelOnNavigationRule } from './rules/aria-label-on-navigation';
 
 const rules = {
+    'dialog-has-aria-label': ariaLabelOnDialogRule,
     'icon-button-has-aria-label': ariaLabelOnIconButtonRule,
     'navigation-has-aria-label': ariaLabelOnNavigationRule,
 } satisfies Record<string, Rule.RuleModule>;
@@ -21,6 +23,7 @@ Object.assign(plugin.configs, {
     recommended: {
         plugins: { vapor: plugin },
         rules: {
+            'vapor/dialog-has-aria-label': 'error',
             'vapor/icon-button-has-aria-label': 'error',
             'vapor/navigation-has-aria-label': 'error',
         },
@@ -28,6 +31,7 @@ Object.assign(plugin.configs, {
     legacy: {
         plugins: ['vapor'],
         rules: {
+            'vapor/dialog-has-aria-label': 'error',
             'vapor/icon-button-has-aria-label': 'error',
             'vapor/navigation-has-aria-label': 'error',
         },
