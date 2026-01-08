56c80ddb36b78aa296ec3e3a5a2466821b18617a
noah.choi <noah.choi@goorm.io>
2026-01-08 13:35:18 +0900

    fix use-vapor-id

--- packages/core/src/hooks/use-vapor-id.ts
+++ packages/core/src/hooks/use-vapor-id.ts
@@ -1,20 +1,9 @@
-import * as React from 'react';
+'use client';
 
-import type { AnyProp } from '~/utils/types';
+import { useId } from '@base-ui-components/utils/useId';
 
-import { useIsoLayoutEffect } from './use-iso-layout-effect';
-
-const useReactId = (React as AnyProp)[' useId '.trim().toString()] || (() => undefined);
-let globalId = 0;
-
-function useVaporId(deterministicId?: string): string {
-    const [id, setId] = React.useState<string | undefined>(useReactId());
-    // React versions older than 18 will have client-side ids only.
-    useIsoLayoutEffect(() => {
-        if (!deterministicId) setId((reactId) => reactId ?? String(globalId++));
-    }, [deterministicId]);
-
-    return deterministicId || (id ? `v-${id}` : '');
+function useVaporId(deterministicId?: string): string | undefined {
+    return useId(deterministicId, 'vapor-ui');
 }
 
 export { useVaporId };
