9b2b1614b64987b7a00bd491d787930cbed496af
noah.choi <noah.choi@goorm.io>
2026-01-20 08:52:30 +0900

    conditional lint error on dialog

--- packages/eslint-plugin-vapor/src/index.ts
+++ packages/eslint-plugin-vapor/src/index.ts
@@ -1,16 +1,21 @@
 import type { Rule } from 'eslint';
 
 import { altTextOnAvatarRule } from './rules/alt-text-on-avatar';
-import { ariaLabelOnDialogRule } from './rules/aria-label-on-dialog';
 import { ariaLabelOnIconButtonRule } from './rules/aria-label-on-icon-button';
 import { ariaLabelOnNavigationRule } from './rules/aria-label-on-navigation';
+import { shouldHaveTitleOnDialogRule } from './rules/should-have-title-on-dialog';
 
 const rules = {
-    'dialog-has-aria-label': ariaLabelOnDialogRule,
     'icon-button-has-aria-label': ariaLabelOnIconButtonRule,
     'navigation-has-aria-label': ariaLabelOnNavigationRule,
     'avatar-has-alt-text': altTextOnAvatarRule,
+    'dialog-should-have-title': shouldHaveTitleOnDialogRule,
 } satisfies Record<string, Rule.RuleModule>;
+
+// Define the flat config (recommended)
+const recommended = Object.fromEntries(
+    Object.keys(rules).map((ruleName) => [`vapor/${ruleName}`, 'error']),
+);
 
 const plugin = {
     meta: {
@@ -18,27 +23,20 @@ const plugin = {
         version: '0.1.0',
     },
     rules,
-    configs: {},
+    configs: {
+        flat: {},
+        legacy: {},
+    },
 };
 
 Object.assign(plugin.configs, {
-    recommended: {
+    flat: {
         plugins: { vapor: plugin },
-        rules: {
-            'vapor/dialog-has-aria-label': 'error',
-            'vapor/icon-button-has-aria-label': 'error',
-            'vapor/navigation-has-aria-label': 'error',
-            'vapor/avatar-has-alt-text': 'error',
-        },
+        rules: recommended,
     },
     legacy: {
         plugins: ['vapor'],
-        rules: {
-            'vapor/dialog-has-aria-label': 'error',
-            'vapor/icon-button-has-aria-label': 'error',
-            'vapor/navigation-has-aria-label': 'error',
-            'vapor/avatar-has-alt-text': 'error',
-        },
+        rules: recommended,
     },
 });
 
