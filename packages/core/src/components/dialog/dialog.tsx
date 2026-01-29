951175f8710ffb917633401ffb8098c0b84aa74a
MaxLee <126851396+MaxLee-dev@users.noreply.github.com>
2026-01-29 15:23:17 +0900

    fix(dialog): correct ReactElement type parameters in DialogPopup props (#438)

--- packages/core/src/components/dialog/dialog.tsx
+++ packages/core/src/components/dialog/dialog.tsx
@@ -246,8 +246,8 @@ export namespace DialogPopupPrimitive {
 
 export namespace DialogPopup {
     export interface Props extends DialogPopupPrimitive.Props {
-        portalElement?: ReactElement<typeof DialogPortalPrimitive>;
-        overlayElement?: ReactElement<typeof DialogOverlayPrimitive>;
+        portalElement?: ReactElement<DialogPortalPrimitive.Props>;
+        overlayElement?: ReactElement<DialogOverlayPrimitive.Props>;
     }
 }
 
