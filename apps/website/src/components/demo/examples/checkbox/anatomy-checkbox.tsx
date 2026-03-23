f7a41f4a05d1b48633bdd6ed6ae44ec584938d82
ChanHyung-Lee <max.lee@goorm.io>
2026-03-23 16:49:53 +0900

    fix(anatomy-checkbox): add aria-label for improved accessibility

--- apps/website/src/components/demo/examples/checkbox/anatomy-checkbox.tsx
+++ apps/website/src/components/demo/examples/checkbox/anatomy-checkbox.tsx
@@ -2,7 +2,7 @@ import { Checkbox } from '@vapor-ui/core';
 
 export default function AnatomyCheckbox() {
     return (
-        <Checkbox.Root data-part="Root" defaultChecked>
+        <Checkbox.Root data-part="Root" defaultChecked aria-label="Anatomy checkbox">
             <Checkbox.IndicatorPrimitive data-part="IndicatorPrimitive" />
         </Checkbox.Root>
     );
