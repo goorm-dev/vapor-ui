80486d8281fb6708a487f4e9f6c69b92b2f09afb
noah.choi <noah.choi@goorm.io>
2026-06-25 17:10:28 +0900

    chore(figma-token-review-plugin): scaffold app

---
+++ apps/figma-token-review-plugin/src/plugin/code.ts
@@ -0,0 +1,1 @@
+figma.showUI(__html__, { width: 450, height: 600 });
---
+++ apps/figma-token-review-plugin/src/ui/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from '@vapor-ui/core';
+
+const App = () => {
+    return (
+        <Box className="bg-white p-v-200">
+            <Text typography="body1">Vapor Token Review (scaffold)</Text>
+        </Box>
+    );
+};
+
+export default App;
