e0ad52af44ec0bea804e714902ad5fff82608e69
noah.choi <noah.choi@goorm.io>
2026-08-20 08:49:36 +0900

    lint & format

--- packages/rolldown-config/src/plugins/deps-external.ts
+++ packages/rolldown-config/src/plugins/deps-external.ts
@@ -1,5 +1,6 @@
 import type { RolldownPlugin } from 'rolldown';
-import depsExternalPlugin, { type ExternalsOptions } from 'rollup-plugin-node-externals';
+import depsExternalPlugin from 'rollup-plugin-node-externals';
+import type { ExternalsOptions } from 'rollup-plugin-node-externals';
 
 export const depsExternal = (options?: ExternalsOptions): RolldownPlugin =>
     depsExternalPlugin(options) as RolldownPlugin;
